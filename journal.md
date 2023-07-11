26740a5c6868167531e378823aa2b14b7c01d52f
craigsanders22 <craig.sanders@me.com>
2023-07-11 10:41:05 -0400

    php practice + journal entry

--- journal.md
+++ journal.md
@@ -7,7 +7,7 @@ A daily worksheet for my DFTW journey.
 
 ### standup 
 
-yesterday - did the 6 icons/symbols and got that posted, made sure MAMP was set up and working correctly, watched and worked through the exercises for the PHP lesson
+yesterday - did the 6 icons/symbols and got that posted, made sure MAMP was set up and working correctly, watched and worked through the exercises for the PHP lesson. Set up my daily journal and got it connected with git, etc. I'm using it this morning to write this! 
 
 today - going to do a bit more from yesterday this morning then get into today's lesson. Meet with Derek later. Hoping to start chipping away at some of the work revolving around my personal site this afternoon. 
 
--- learning/index.php
+++ learning/index.php
@@ -7,10 +7,80 @@
 			}
 
 			p {
+				
 				color:slateblue;
 			}
 		</style>
 	</head>
+
+	<!-- practice area -->
+
+
+		<p><?php echo "Hello, World";?></p>
+
+		<h2> <?php echo "HELLOOOOO, WORLD!!!";?></h2>
+
+		<p><?php echo 22;?></p>
+
+
+		<p><?php echo 22 + 22;?></p>
+
+		<?php
+
+		$name = "Craig";
+		$country = "Spain";
+		$sentence = "Hello, my name is " . $name . " and I love " . $country . ".";
+
+		echo $sentence;
+
+		?>
+
+
+		<p><?="What do you want? ", 100;?></p>
+
+		<?php
+
+		$itemone = 20;
+		$itemtwo = 12;
+
+		$total = $itemone + $itemtwo;
+
+
+		echo $total;
+
+		$referencetotext = " is your total.";
+
+		echo $referencetotext;
+
+		?>
+
+
+		<?php 	
+
+			$brandName = "Jalapeno Poppers";
+
+
+
+		 ?>
+
+		 <p>I was driving down the highway and I saw a sign for <?=$brandName?> so I knew I had to stop. Sometimes I really love to get a treat from <?=$brandName?>. Sadly, this time the food from <?=$brandName?> was super soggy. Sad.</p>
+
+
+		 <?php 
+
+		 	$website = "https://cooking.nytimes.com/recipes/1020843-jalapeno-poppers";
+
+		 	?>
+
+		 <h2>I guess I'll just have to make them myself using this <a href=<?=$website;?>>recipe</a>.</h2>
+
+
+
+
+
+<!-- mad lib -->
+
+
 
 	<body>
 		<header>
@@ -38,12 +108,12 @@
 
 
 
-		 <p> Every <?=$month?> <?=$number?> we always have a picnic at <?=$place?>  We invite <?=$nounOne?> and <?=$nounTwo?>. It is next to a very <?=$adjectiveOne?> lake. </p>
+		 <p> Every <?=$month?> <?=$number?> we always have a birthday picnic at <?=$place?>  We invite <?=$nounOne?> and <?=$nounTwo?>. It is next to a very <?=$adjectiveOne?> lake. </p>
 			
 
 		<p> We have to walk <?=$adverbOne?> to get there without <?=$verbOne?>. There are <?=$animalOne?> and <?=$animalTwo?> which are very <?=$adjectiveTwo?>. We eat lots of food that everyone <?=$adjectiveThree?>. The games are my favorite part.</p>
 
-		<p>We play <?=$verbTwo?>ing and <?=$verbThree?>ing until we can’t walk anymore. Then we <?=$verbFour?> back and relax under the old shade tree until <?=$nounOne?>says, Happy Birthday!</p>"
+		<p>We play <?=$verbTwo?>ing and <?=$verbThree?>ing until we can’t walk anymore. Then we <?=$verbFour?> back and relax under the old shade tree until <?=$nounOne?> says, "Happy Birthday!"</p>
 		
 	
 		
