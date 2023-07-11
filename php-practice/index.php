77759e724ecf12cd0204056bec29505a5465e06c
craigsanders22 <craig.sanders@me.com>
2023-07-11 11:13:37 -0400

    initial commit - php practice

---
+++ php-practice/index.php
@@ -0,0 +1,16 @@
+<h1>PHP Practice</h1>
+
+<?php
+
+	$first = "Craig";
+	$last = "Sanders";
+
+	$full_name = $first . " " . $last;
+
+	$age = 5 + 6 + 7 + 8 + 9;
+
+?>
+
+
+<p>My name is <?=$full_name?>. I am <?=$age?> years old!</p>
+
