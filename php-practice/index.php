fb330c05bd3ce463b08f9db4c67adc21c25fd06b
craigsanders22 <craig.sanders@me.com>
2023-07-11 20:55:15 -0400

    fixed

--- php-practice/index.php
+++ php-practice/index.php
@@ -1,58 +1,60 @@
+<h1>PHP Practice</h1>
+
 <?php
 
-	// defaults
-	$first = "Craig"; // 1, true, "Craig"
+	$first = "Craig";
 	$last = "Sanders";
 
-	// user interaction 
-	if ( !isset($first) )  {
-		$first = "Default"; // example of a form maybe?
+	if ($first) {
+
+	} else {
+		$first = "Default";
 	}
 
 	$full_name = $first . " " . $last;
 
-	$age = 5 + 6 + 7 + 8 + 9; // 2023 - 1988 === $day
+	$age = 5 + 6 + 7 + 8 + 9;
 
-	$myData = date("Y"); // https://www.php.net/manual/en/function.date.php
+?>
 
-	$yearsLived = data("Y") - 1988;
+
+<p>My name is <?=$full_name?>. I am <?=$age?> years old!</p>
+
+<hr>
+
+<!-- Practice -->
+<hr>
+
+<?php
 
 	$stock = rand(-2, 5);
 
 	if ($stock > 0) {
 		$message = 'In stock';
 	} else {
-		$message = 'Sorry, we are SOLD OUT for today! :(';
-	}
-
-	// $message = 'Sorry, we are SOLD OUT for today! :(';
-
-	// if ($stock > 0) {
-	// 	$message = 'In stock';
-	// }
+			$message = 'Sorry, we are SOLD OUT for today! :(';
+		}
 
 	if ($stock > 0) {
 		$order = '<a href="https://www.papajohns.com/order/menu/sides/jalapeno-popper-rolls?displayNutritionalInfo=false";?>Order Here!</a>';
 	} else {
 		
-	}
-?>
 
-<html>
-	<body>
+		}
+	
 
-		<h1>PHP Practice</h1>
+	?>
 
-		<p>My name is <?=$full_name?>. I am <?php echo $age; ?> years old!</p>
+	<html>
+		<body>
+			<h1>Popper Shop</h1>
+			<h2>The World's Best Jalapeno Poppers</h2>
+			<p>Our stock is currently <?=$stock?> poppers!</p>
+			<p><?= $message ?></p>
+			<p><?= $order ?></p>
+		</body>
+	</html>
 
-		
-		<h1>Popper Shop</h1>
 
-		<h2>The World's Best Jalapeno Poppers</h2>
 
-		<p>Our stock is currently <?=$stock?> poppers!</p>
-		<p><?= $message ?></p>
-		<p><?= $order ?></p>
 
-	</body>
-</html>
