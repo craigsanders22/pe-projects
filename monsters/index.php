2bd189ea687001d3477475bad3c550884cbb6c04
craigsanders22 <craig.sanders@me.com>
2023-07-14 10:55:42 -0400

    final product

--- monsters/index.php
+++ monsters/index.php
@@ -1,30 +1,44 @@
 
-
-
 <style>
 
-	* {
+
+	img {
 		display: block;
-	
+		width: 100%;
+		height: auto;
+		max-width: 400px;
+		overflow: hidden;
+	}
+
+	.inner-column {
+		max-width: 700px;
+		padding: 20px;
+		margin-left: auto;
+		margin-right: auto;
+		
 	}
 
 	ul {
 
 		display: flex;
-		flex-direction: row;
+		flex-direction: column;
 		margin-right: auto;
 		margin-left: auto;
+		
 		list-style-type: none;
+
 
 	}
 
 
-	monster-card{
-		display: block;
+	monster-card {
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+		align-items: center;
 		border: 10px solid transparent;
-  border-image: linear-gradient(to bottom right, #b827fc 0%, #2c90fc 25%, #b8fd33 50%, #fec837 75%, #fd1892 100%);
-  border-image-slice: 1;
-		max-width: 400px;
+  		border-image: linear-gradient(to bottom right, #b827fc 0%, #2c90fc 25%, #b8fd33 50%, #fec837 75%, #fd1892 100%);
+  		border-image-slice: 1;
 		padding: 20px;
 	}
 
@@ -32,7 +46,6 @@
 
 
 </style>
-
 
 <?php 
 
@@ -137,6 +150,7 @@ echo "<li class = 'monster'>";
 
 
 echo 
+	"<div style='" . $monster . "' class='inner-column'>" .
 	"<monster-card id='" . $id . "'>" .
 		"<picture class='portrait'>" . 
 			"<img src='" . $portrait . "' width='300'>" .
