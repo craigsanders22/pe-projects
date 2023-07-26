80d98e070a911efda3b4b1a3066e9b1ef309f562
craigsanders22 <craig.sanders@me.com>
2023-07-26 18:43:17 -0400

    header adjustment

--- flower-zone/site-menu.php
+++ flower-zone/site-menu.php
@@ -1,9 +1,11 @@
 <div class="inner-column">
 <nav class='site-menu'>
 	
-	
-	<h3><a href="?page=home">Home</a></h3>
-	<h3><a href="?page=flowers">Our Flowers</a></h3>
+	<img src="assets/flower-zone.svg">
+	<div class="links">
+		<h3><a href="?page=home">Home</a></h3>
+		<h3><a href="?page=flowers">Our Flowers</a></h3>
+	</div>
 
 
 
