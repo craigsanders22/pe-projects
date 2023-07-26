812aa33d0b869ee9790f7a21b10062da0327c050
craigsanders22 <craig.sanders@me.com>
2023-07-26 18:23:55 -0400

    text to home

--- flower-zone/home.php
+++ flower-zone/home.php
@@ -1,8 +1,3 @@
-<style>
-	.inner-column h1 {
-		padding-left: 20px;
-	}
-</style>
 
 
 
@@ -14,9 +9,41 @@
 	<title>Document</title>
 </head>
 <body>
+	<main class=intro>
 	<div class="inner-column">
+		<section class=headline>
+
+
 	<h1>Welcome to the Flower Zone! </h1>
+
+
+		</section>
+
+		<section class="about">
+			<h2>About us</h2>
+			<p>What we're about. Flowers, I guess. We grow flowers. Sell flowers. Dry flowers. Oh, and jalapeno poppers. </p>
+
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis rem, ex ipsam molestiae quidem atque deleniti, aut autem placeat omnis in cumque odit harum. Officia explicabo, rem porro eaque sunt. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora autem totam obcaecati rerum cumque officiis assumenda adipisci, ex! Sapiente, veritatis. Expedita itaque similique aperiam vero voluptatibus sapiente mollitia veniam autem.</p>
+
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae ratione nostrum sint aliquam, corrupti, suscipit inventore nam mollitia sunt quis voluptatum nesciunt a eum cupiditate et est, sapiente, fugit eos.</p>
+
+			<p>Lorem ipsum, dolor sit amet consectetur, adipisicing elit. Necessitatibus quaerat distinctio pariatur modi adipisci obcaecati dolor. Animi, ipsa esse dolorem ex nemo. Voluptatibus quidem obcaecati vitae quam voluptas nam quae. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste saepe deserunt, et molestiae laboriosam fugiat facere voluptas iusto unde harum pariatur minima doloribus quidem non exercitationem, soluta earum perspiciatis expedita. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam, inventore asperiores earum odio esse eaque eligendi, totam? Nihil quasi, sint impedit facilis dolores laudantium consectetur velit, quod nulla sit distinctio.</p>
+			</section>
+
+			<section class="about">
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit dolorem ad sit esse illum quae sed exercitationem pariatur, rem! At molestiae aliquam odit numquam voluptatibus repudiandae. Officiis, quia doloribus. Facere!</p>
+
+				<p>Lorem ipsum, dolor sit amet consectetur, adipisicing elit. Necessitatibus quaerat distinctio pariatur modi adipisci obcaecati dolor. Animi, ipsa esse dolorem ex nemo. Voluptatibus quidem obcaecati vitae quam voluptas nam quae. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste saepe deserunt, et molestiae laboriosam fugiat facere voluptas iusto unde harum pariatur minima doloribus quidem non exercitationem, soluta earum perspiciatis expedita. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam, inventore asperiores earum odio esse eaque eligendi, totam? Nihil quasi, sint impedit facilis dolores laudantium consectetur velit, quod nulla sit distinctio.</p>
+
+
+			</section>
+
+
+
+
 	</div>
+</main>
 </body>
 </html>
 
