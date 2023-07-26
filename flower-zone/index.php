c76e811a49337546f6fcd74db3b954ffc34a2c7f
craigsanders22 <craig.sanders@me.com>
2023-07-26 09:24:33 -0400

    initial

---
+++ flower-zone/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<!--
+	pages: home, flowers, detail
+-->
+
+
+
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Flower Zone</title>
+</head>
+
+
+<body>
+
+	<h1>Welcome to the Flower Zone! </h1>
+
+	<nav class='site-menu'>
+		<a href="?page=home">Home</a>
+		<a href="?page=flowers">Our Flowers</a>
+		
+
+
+	</nav>
+	
+</body>
+</html>
