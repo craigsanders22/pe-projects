4f4b995959684752e3c87ba800d34a8337da4a86
craigsanders22 <craig.sanders@me.com>
2023-07-27 11:50:16 -0400

    form update

--- flower-zone/create.php
+++ flower-zone/create.php
@@ -1,13 +1,90 @@
 <?php 
 
+	
+		$name = "";
+		$breed = "";
+		$type = "";
+		$season = "";
+		$colors = "";
+		$price = 0;
+
+
+
+
+
+
 	if ( isset($_POST["add"]) ) {
 		
-		$hasName = $_POST["Flower Name"];
+
+		
+		if ( isset($_POST["name"]) ) {
+			$name = $_POST["name"];
+
+		if ( strlen($name) > 0) {
+			$hasName = true;
+			}
+
+		}
 
 
+		
+		if ( isset($_POST["breed"]) ) {
+			$breed = $_POST["breed"];
 
-		echo "added";
-	}
+		if ( strlen($breed) > 0) {
+			$hasBreed = true;
+			}
+
+		}
+
+
+		
+		if ( isset($_POST["type"]) ) {
+			$type = $_POST["type"];
+
+		if ( strlen($type) > 0) {
+			$hasType = true;
+			}
+
+		}
+
+		
+		if ( isset($_POST["season"]) ) {
+			$season = $_POST["season"];
+
+		if ( strlen($season) > 0) {
+			$hasSeason = true;
+			}
+
+		}
+
+		
+		if ( isset($_POST["colors"]) ) {
+			$colors = $_POST["colors"];
+
+		if ( strlen($colors) > 0) {
+			$hasColors = true;
+			}
+
+		}
+
+		
+		if ( isset($_POST["price"]) ) {
+			$price = $_POST["price"];
+
+		if ( strlen($price) > 0) {
+			$hasPrice = true;
+			}
+
+		}
+
+		if ($hasSeason && $hasPrice) {
+			echo "Has the goods!";
+		} else {
+			echo "no good!";
+		}
+
+		}
 
  ?>
 
@@ -20,38 +97,42 @@
 
 	<field>
 		<label>Flower Name</label>
-		<input name='Flower Name'>
+		<input type="text" name='name' value='<?=$name?>'>
+	
 	</field>
 
 
 	<field>
 		<label>Breed</label>
-		<input type="text" name='breed'>
+		<input type="text" name='breed' value='<?=$breed?>'>
 	</field>
 
 
 	<field>
 		<label>Plant Type</label>
-		<input type="text" name='plant-type'>
+		<input type="text" name='type' value='<?=$type?>'>
 	</field>
 
 
 	<field>
 		<label>Growing Season</label>
-		<input type="text" name='growing-season'>
+		<input type="text" name='season' value='<?=$season?>'>
 	</field>
 
 	<field>
 		<label>Color</label>
-		<input type="text" name='colors'>
+		<input type="text" name='colors' value='<?=$colors?>'>
 	</field>
 
 	<field>
 		<label>Price</label>
-		<input type="number" name='price'>
+		<input type="number" name='price' value='<?=$price?>'>
 	</field>
 
+	<field>
+		
 	<button type="submit" name="add">
 		Submit
 	</button>
+	</field>
 </form>
