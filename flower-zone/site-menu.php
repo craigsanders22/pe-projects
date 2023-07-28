42fefe7017661ce3ac3d593584fd7ee099084352
craigsanders22 <craig.sanders@me.com>
2023-07-28 10:13:04 -0400

    style header

--- flower-zone/site-menu.php
+++ flower-zone/site-menu.php
@@ -3,9 +3,9 @@
 	
 	<img src="assets/flower-zone.svg">
 	<div class="links">
-		<h3><a href="?page=home">Home</a></h3>
-		<h3><a href="?page=flowers">Our Flowers</a></h3>
-		<h3><a href="?page=create">Staff Only</a></h3>
+		<h3><a href='?page=home' class='<?php if ($page == "home") {echo "active"; } ?>'>Home</a></h3>
+		<h3><a href="?page=flowers" class='<?php if ($page == "flowers") {echo "active"; } ?>'>Our Flowers</a></h3>
+		<h3><a href="?page=create" class='<?php if ($page == "create") {echo "active"; } ?>'>Staff Only</a></h3>
 
 	</div>
 
