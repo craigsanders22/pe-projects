8771a97773fe0ecb938455636e3d117066adb7d3
craigsanders22 <craig.sanders@me.com>
2023-07-30 22:25:39 -0400

    JSON started

--- flower-zone/create.php
+++ flower-zone/create.php
@@ -1,68 +1,46 @@
 <?php 
+
+		$theFlowers = file_get_contents('flowers.json');
+
+		var_dump($theFlowers);
 		$name = "";
-		$breed = "";
-		$type = "";
-		$season = "";
-		$colors = "";
-		$price = 0;
-		$priceError = false; 
-
-	if ( isset($_POST["add"]) ) {
+		$hasName = false;
+		$nameError = false; 
 		
-		if ( isset($_POST["name"]) ) {
-			$name = $_POST["name"];
 
-		if ( strlen($name) > 0) {
-			$hasName = true;
-			}
-		}
-
-		if ( isset($_POST["breed"]) ) {
-			$breed = $_POST["breed"];
-
-		if ( strlen($breed) > 0) {
-			$hasBreed = true;
-			}
-		}
-
-		if ( isset($_POST["type"]) ) {
-			$type = $_POST["type"];
-
-		if ( strlen($type) > 0) {
-			$hasType = true;
-			}
-		}
-
-		if ( isset($_POST["season"]) ) {
-			$season = $_POST["season"];
-
-		if ( strlen($season) > 0) {
-			$hasSeason = true;
-			}
-		}
-
-		if ( isset($_POST["colors"]) ) {
-			$colors = $_POST["colors"];
-
-		if ( strlen($colors) > 0) {
-			$hasColors = true;
-			}
-		}
+	$formSubmitted = isset($_POST["add"]);
 		
-		if ( isset($_POST["price"]) ) {
-			$price = $_POST["price"];
+		if ($formSubmitted) {
 
-		if ( intval($price) > 0 ) {
-			$hasPrice = true;
+			if ( isset($_POST["name"] ) ){
+				$name = $_POST['name'];
+
+			if ( strlen($name) > 0) {
+				$hasName = true;
+
+			
+
+
+			$newFlower = [
+				"name" => $name,
+			];
+
+			var_dump($newFlower);
+
+				//create montster
+
+			//pull up the JSON
+
+			$flowerJson = json_encode($newFlower);
+
+			// save monster
+
+			file_put_contents('flowers.json', $flowerJson);
+
+
 			} else {
-				$priceError = "Please add a price.";
+				$nameError = "Please add a name.";
 			}
-		}
-
-		if ($hasSeason && $hasColors) {
-			echo "Has the goods!";
-		} else {
-			echo "no good!";
 		}
 	}
 
@@ -83,34 +61,10 @@
 		<input type="text" name='name' value='<?=$name?>'>	
 	</field>
 
-	<field>
-		<label>Breed</label>
-		<input type="text" name='breed' value='<?=$breed?>'>
-	</field>
-
-	<field>
-		<label>Plant Type</label>
-		<input type="text" name='type' value='<?=$type?>'>
-	</field>
-
-	<field>
-		<label>Growing Season</label>
-		<input type="text" name='season' value='<?=$season?>'>
-	</field>
-
-	<field>
-		<label>Color</label>
-		<input type="text" name='colors' value='<?=$colors?>'>
-	</field>
-
-	<field>
-		<label>Price</label>
-		<input type="number" name='price' value='<?=$price?>'>
-	</field>
 
 	<field>	
 		
-	<button type="submit" name="add">
+	<button type="submit" name='add'>
 		Submit
 	</button>
 
