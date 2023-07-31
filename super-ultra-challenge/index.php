d49918e5ec771509145a567e857f38efd8755f7a
craigsanders22 <craig.sanders@me.com>
2023-07-31 13:11:41 -0400

    update files

--- super-ultra-challenge/index.php
+++ super-ultra-challenge/index.php
@@ -1,11 +1,35 @@
 <!DOCTYPE html>
+
+
+<?php 
+
+	$page = null;
+	if ( isset($_GET["page"]) ) {
+		$page = $_GET["page"];
+	} else {
+		$page = "home";
+	}
+
+	function getTemplate($page) {
+		include($page . '.php');
+	}
+ ?>
+
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Super Ultra Responsive</title>
+	<link rel="stylesheet" href="css/site.css">
+
 </head>
 <body>
+<header>
+		<?php include('masthead.php'); ?>
+
+</header>
+
+
 	
 </body>
 </html>
---
+++ super-ultra-challenge/masthead.php
@@ -0,0 +1,19 @@
+<header class="site-header">
+	<inner-column>
+
+		<masthead>
+			<nav class="site-menu">
+				<a href="#">LOGO</a>
+				<a href="#">A Page</a>
+				<a href="#">Another Page</a>
+			</nav>
+
+			<nav class="sign-in">
+				<a href="#">Sign-in</a>
+
+			</nav>
+		</masthead>
+
+	</inner-column>
+
+</header>
