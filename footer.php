785144685be91ed5f441f68fd3887332f86dedcb
craigsanders22 <craig.sanders@me.com>
2023-08-09 17:31:28 -0400

    cleaned head and foot

--- footer.php
+++ footer.php
@@ -1,21 +1,20 @@
+  </main>
 
-<footer class="site-footer">
-  <nav-class="site-nav">
-    <div class="inner-column">
-<nav class="site-nav">
-      <ul class="nav-list">
+  
+  <footer class="site-footer">
+      <div class="inner-column">
 
-        <li><a href="index.php"> home</a></li>
-        <li><a href="projects.php"> projects</a></li>
-        <li><a href="goals.php"> my goals</a></li>
-        <li><a href="resume.php"> resume</a></li>
-        <li><a href="contact.php"> let's chat</a> </li>
+        <nav class="site-nav">
+          <ul class="nav-list">
+            <li><a href="index.php"> home</a></li>
+            <li><a href="projects.php"> projects</a></li>
+            <li><a href="goals.php"> my goals</a></li>
+            <li><a href="resume.php"> resume</a></li>
+            <li><a href="contact.php"> let's chat</a> </li>
+          </ul>
+        </nav>
 
-      </ul>
-
-    </nav>
-    </div>
-    </div>
-</footer>
-
+      </div>
+  </footer>
+</body>
 </html>
