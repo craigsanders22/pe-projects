2940831e271d427b7761f18f0c942eade2e1fa8e
craigsanders22 <craig.sanders@me.com>
2023-08-10 10:17:47 -0400

    links/nav

--- header.php
+++ header.php
@@ -11,13 +11,14 @@
       <div class="header-items">
         <div class="logo">
           <picture>
+          	<a href="../#">
            <img src="../assets/alpha-seven.png"/>
           </picture>
         </div>
     
         <nav class="site-nav">
           <ul class="nav-list">
-            <li><a href="index.php">home</a></li>
+            <li><a href="../index.php">home</a></li>
             <li><a href="../projects">projects</a></li>
             <li><a href="../goals">my goals</a></li>
             <li><a href="../resume">resume</a></li>
