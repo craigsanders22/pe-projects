c8b37f99c470ad0891ad70dc6a3214f853efa9d3
craigsanders22 <craig.sanders@me.com>
2023-08-13 18:46:50 -0400

    v

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
+<!DOCTYPE html>
 
+<?php 
+
+  $page = null;
+  if ( isset($_GET["page"]) ) {
+    $page = $_GET["page"];
+  } else {
+    $page = "home";
+  }
+
+  function getTemplate($page) {
+    include($page . '.php');
+  }
+ ?>
+
+
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Craig Sanders</title>
+  <link rel="stylesheet" href="styles/site.css">
+</head>
+<body>
+  
 
   <?php include('header.php');?>
 
--- projects/index.php
+++ projects/index.php
@@ -1,11 +1,4 @@
-<?php function showErrors() {
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  error_reporting(E_ALL);
-}
 
-showErrors()
- ?>
 
 <?php include('../header.php');?>
 
