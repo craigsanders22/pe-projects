f5587326a69d81abc9b1fb74d2992dfa7adb5b0c
craigsanders22 <craig.sanders@me.com>
2023-08-14 06:35:54 -0400

    setting up home PHP

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
         <nav class="site-nav">
           <ul class="nav-list">
             <li><a href="../index.php">home</a></li>
-            <li><a href="../projects">projects</a></li>
+            <li><a href="/projects">projects</a></li>
             <li><a href="../goals">my goals</a></li>
             <li><a href="../resume">resume</a></li>
             <li><a href="../contact">let's chat</a> </li>
