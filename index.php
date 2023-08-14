1a212047ee0bcc77fba3551b875db8aeb0d73b5e
craigsanders22 <craig.sanders@me.com>
2023-08-14 06:46:15 -0400

    .

--- index.php
+++ index.php
@@ -15,32 +15,17 @@
  ?>
 
 
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Craig Sanders</title>
-  <link rel="stylesheet" href="styles/site.css">
-</head>
-<body>
-  <header>
-
   <?php include('header.php');?>
-</header>
+
 
 <main>
   <?php include('home.php'); ?>
   <?php include('projects/index.php'); ?>
   <?php include('resume.php'); ?>
   <?php include('goals.php'); ?>
-  <?php include('../contact.php'); ?>
+  <?php include('contact.php'); ?>
 
 
-</main>
-</body>
-</html>
-
-  
 
 <?php
  
