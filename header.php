4cf0f1df3e66c745ab07d9e5066b730619879f66
craigsanders22 <craig.sanders@me.com>
2023-08-14 07:04:01 -0400

    b

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
       <div class="header-items">
         <div class="logo">
           <picture>
-          	<a href="../#">
+          	<a href="#">
            <img src="../assets/alpha-seven.png"/>
           </picture>
         </div>
@@ -19,9 +19,9 @@
         <nav class="site-nav">
           <ul class="nav-list">
            
-            <li><a href="../projects">projects</a></li>
-            <li><a href="../goals">my goals</a></li>
-            <li><a href="../resume">resume</a></li>
+            <li><a href="projects">projects</a></li>
+            <li><a href="goals">my goals</a></li>
+            <li><a href="resume">resume</a></li>
             <li><a href="contact">let's chat</a> </li>
           </ul>
         </nav>
