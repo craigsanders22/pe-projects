7ab9d6165a759adc3fe02b5e80a2691b009e75c4
craigsanders22 <craig.sanders@me.com>
2023-08-20 17:31:46 -0400

    g

--- head.php
+++ head.php
@@ -9,7 +9,7 @@
   <meta property="og:type" content="TYPE">
   <meta property="og:url" content="https://peprojects.dev/alpha-7/craig/">
   <meta property="og:description" content="Craig Sanders is a web designer based in Indianapolis, Indiana">
-  <meta property="og:image" content="assets/meta.jpg">
+  <meta property="og:image" content="assets/meta.png">
 
   <link rel="stylesheet" href='styles/site.css'>
 
