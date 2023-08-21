5ba8ea4bb2c36dc473ec9a776ff73f805154f61f
craigsanders22 <craig.sanders@me.com>
2023-08-21 07:21:04 -0400

    fixing home styles

--- home.php
+++ home.php
@@ -1,12 +1,20 @@
 <section class="intro">
   <div class="inner-column">
     
-    <?php include("modules/welcome.php") ?>
+    <h1 class='loud-voice'>
+      Hey there,
+
+      I'm Craig.
+    </h1>
+  </div>
+
+  <div class="avatar">
+    <img src="assets/avataaars.png">
 
   </div>
 </section>
 
-  <section>
+  <section class="about">
     <div class="about">
       <div class="inner-column">
 
@@ -25,6 +33,7 @@
 
       </div>
     </div>
+  </section>
 
     <div class="accomplishments">
       <div class="inner-column">
@@ -68,13 +77,13 @@
       </div>
     </div>
 
-    <div class="favorite-things">
+    <section class="favorite-things">
       <div class="inner-column">
 
-        <h2>some of my<br> favorite things <br>
+        <h2 class='attention-voice'>some of my<br> favorite things <br>
           (right now)</h2>
 
-        <p>
+        <p class="calm-voice">
         <ul>
           <li>Creating visually-pleasing pages & apps</li>
           <li>Making responsive and functional sites</li>
@@ -88,7 +97,7 @@
           <li>Learning about the next big thing</li>
         </ul>
         </p>
-
+    
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300">
           <path fill="#c7d6e8" d="M34.15 41.6H265.84999999999997V255.26999999999998H34.15z"></path>
           <path fill="#7e99bb" opacity="0.2" d="M265.85 41.6L34.15 41.6 34.15 136.43"></path>
@@ -98,7 +107,10 @@
         </svg>
       </div>
     </div>
-    <div class="contact">
+  </section>
+
+
+    <section class="contact">
       <div class="inner-column">
         <div class="contact-text">
 
@@ -107,6 +119,7 @@
           <p>Let’s chat about my journey, your journey, what kinds of jobs I'll be looking for, what kind of jobs you’re hiring/looking for, your favorite sandwich condiment, or anything else that we might have in common! Please don't hesitate to reach out!</p>
         </div>
       </div>
+      
       <div class=contact-cards>
         <div class="link">
 
