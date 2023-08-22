bd6c61ffd67eac3fe23af99f4136ab687e8922fd
craigsanders22 <craig.sanders@me.com>
2023-08-22 12:56:54 -0400

    projects css

--- projects/project-cards/template.php
+++ projects/project-cards/template.php
@@ -16,14 +16,14 @@
 <div class="inner-column">
 <project class='project-card'>
 	<picture>
-		<img src='../assets/<?=$thumbnail?>' alt='$todo'>
+		<img src='/assets/<?=$thumbnail?>' alt='$todo'>
 	</picture>
 
 	<text-content>
 		<h1 class='strong-voice'><?=$heading?></h1>
 
 		<p class='teaser'><?=$description?></p>
-		<a href='../projects/<?=$link?>'> Find out More!</a>
+		<a href='/projects/<?=$link?>'> Find out More!</a>
   
 	</text-content>
 </project>
