d72bd3869081b977ba3295a36774a875e274f24a
craigsanders22 <craig.sanders@me.com>
2023-08-22 09:26:08 -0400

    v

--- header.php
+++ header.php
@@ -21,8 +21,8 @@
         <nav class="site-nav">
           <ul class="nav-list">
             
-            <li><a href="projects/index.php">projects</a></li>
-            <li><a href="goals/index.php">my goals</a></li>
+            <li><a href="projects/">projects</a></li>
+            <li><a href="goals/">my goals</a></li>
             <li><a href="resume/">resume</a></li>
             <li><a href="contact/">let's chat</a> </li>
           </ul>
