95b0be08910f3402de4831cbada12c30cc162beb
craigsanders22 <craig.sanders@me.com>
2023-08-22 16:49:47 -0400

    content left inside column

--- resume/index.php
+++ resume/index.php
@@ -1,14 +1,15 @@
 <?php include('../header.php');?>
 
-
 <main class="resume">
-  <div class="intro">
-    <inner-column>
+
+  <section class="intro">
+
     <h1 class="loud-voice">Craig Sanders</h1>
     <p class="calm-voice">Indianapolis, IN 46219 | craig.sanders@me.com | +1 765 721 0820</p>
-  </div>
-    
-    <div class="work-experience">
+  </section>
+
+ <div class="inner-column">
+    <section class="work-experience">
       <h2 class="attention-voice">Work Experience</h2>
       <div class="work-details">
         <h3 class="work-title strong-voice ">Small Business Owner / Operator</h3>
@@ -17,33 +18,42 @@
         <ul>
           <li>Established and managed a successful family-owned specialty market, supporting local farmers and food brands.</li>
           <li>Curated a thoughtful selection of fresh and packaged food products in the Fountain Square neighborhood.</li>
-        
+
         </ul>
       </div>
-      
+
       <div class="work-details">
-        <div class="work-title">Director of Ops</div> 
-        <div class="work-title">Director of Sales and Business Development</div>
+        <div class="work-title strong-voice">Director of Ops</div>
         <div>Traders Point Creamery - Zionsville, IN</div>
-        <div class="work-dates">June 2007 - September 2014</div>
+        <div class="work-dates"> August 2010- September 2014</div>
         <ul>
           <li>Transformed a local organic dairy brand into a nationally distributed product available in grocery and specialty chains.</li>
           <li>Cultivated and maintained relationships with national distributors, leading to increased market presence.</li>
-          
         </ul>
       </div>
-    </div>
-    
-    <div class="education">
-      <h2 class= "attention-voice">Education</h2>
+
+      <div class="work-details">
+        <div class="work-title strong-voice">Director of Sales and Business Development</div>
+        <div>Traders Point Creamery - Zionsville, IN</div>
+        <div class="work-dates">June 2007 - July 2010</div>
+        <ul>
+          <li>Transformed a local organic dairy brand into a nationally distributed product available in grocery and specialty chains.</li>
+          <li>Cultivated and maintained relationships with national distributors, leading to increased market presence.</li>
+        </ul>
+      </div>
+
+    </section>
+
+    <section class="education">
+      <h2 class="attention-voice">Education</h2>
       <div class="education-details">
         <div>Indiana University-Purdue University Indianapolis - Indianapolis, IN</div>
         <div>English</div>
         <div>August 2003 - May 2006</div>
       </div>
-    </div>
-    
-    <div class="skills">
+    </section>
+
+    <section class="skills">
       <h2 class="attention-voice">Skills</h2>
       <ul class="skills-list">
         <li class="skills-item">Relationship Building and Sales</li>
@@ -51,8 +61,11 @@
         <li class="skills-item">Budgeting and Forecasting</li>
         <li class="skills-item">Sales and Marketing Strategy</li>
       </ul>
-    </div>
-    </inner-column>
+
+    </section>
   </div>
+</div>
+
+</main>
 
   <?php include('../footer.php');?>
