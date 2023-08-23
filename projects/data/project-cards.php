32f980863b4e3885664261fcb5aded29241aa746
craigsanders22 <craig.sanders@me.com>
2023-08-22 20:58:00 -0400

    v

--- projects/data/project-cards.php
+++ projects/data/project-cards.php
@@ -3,15 +3,15 @@ $projects = [
     [
         "heading" => "Exercises For Programmers",
         "description" => "Exercises for Programmers is a book by Brian P. Hogan that includes practice exercises rooted in real-world scenarios. Here are some of my results:",
-        "thumbnail" => "assets/applications.png",
+        "thumbnail" => "applications.png",
         "link" => "../e4p"
     ],
     [
         "heading" => "Responsive Theme Challenge",
         "description" => "In this sprint we were tasked with identifying a real-world product to research, analyze, create style tiles, then make a few iterations of our //stolen// theme. I chose to focus on a masculine-focused line of candles. Using only HTML, CSS, and PHP I designed the site, fleshed out the product and copy, and then built the finishsed site to resemble those that I researched.",
 
-        "thumbnail" => "assets/bonfire.png",
-        "link" => "projects/theme-challenge"
+        "thumbnail" => "../bonfire.png",
+        "link" => "theme-challenge/"
 
     ]
 ];
