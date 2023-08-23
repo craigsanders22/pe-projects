f6ba7ceae78824a29b0dafd41ae40e7d78132093
craigsanders22 <craig.sanders@me.com>
2023-08-23 10:55:10 -0400

    adding archive

---
+++ archive/index.php
@@ -0,0 +1,6 @@
+<?php include('../header.php');?>
+
+<h1>Here are some less polished projects that I've completed</h1>
+<a href="archive/super-ultra-challenge/">Super Ultra Challenge</a>
+
+<?php include('../footer.php');?>
--- footer.php
+++ footer.php
@@ -7,11 +7,12 @@
 
         <nav class="site-nav">
           <ul class="nav-list">
-            <li><a href="index.php">home</a></li>
             <li><a href="projects">projects</a></li>
             <li><a href="goals">my goals</a></li>
             <li><a href="resume">resume</a></li>
             <li><a href="contact">let's chat</a> </li>
+            <li><a href="archive">the vault</a> </li>
+
           </ul>
         </nav>
 
