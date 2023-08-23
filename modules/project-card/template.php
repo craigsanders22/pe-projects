6eddff0d7de2a639c89f8538a218f37d16edddf1
craigsanders22 <craig.sanders@me.com>
2023-08-22 20:35:36 -0400

    fixing project icon routing

--- modules/project-card/template.php
+++ modules/project-card/template.php
@@ -4,14 +4,14 @@ $projects = [
 	[
 		"heading" => "Exercises For Programmers",
 		"description" => "Exercises for Programmers is a book by Brian P. Hogan that includes practice exercises rooted in real-world scenarios. Here are some of my results:",
-		"thumbnail" => "e4p.jpeg",
+		"thumbnail" => "assets/e4p.jpeg",
 		"link" => "../projects/e4p",
 	],
 
 	[
 		"heading" => "Responsive Theme Challenge",
 		"description" => "Exercises for Programmers is a book by Brian P. Hogan that includes practice exercises rooted in real-world scenarios. Here are some of my results:",
-		"thumbnail" => "php.jpg",
+		"thumbnail" => "assets/php.jpg",
 		"link" => "../projects/e4p",
 	],
 
