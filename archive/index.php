de7a0d9de061e85e42a8ad22ea844fff824da356
craigsanders22 <craig.sanders@me.com>
2023-08-28 14:32:19 -0400

    moving files

--- archive/index.php
+++ archive/index.php
@@ -7,11 +7,16 @@
 		<h1 class="attention-voice">Here are some less polished projects that I've completed</h1>
 	</section>
 	<section class="project-grid">
+
+		<a href="archive/medley-week/" target="blank">Medley Week</a>
+		
 		<a href="archive/super-ultra-challenge/" target="blank">Super Ultra Challenge</a>
 
 		<a href="archive/flower-zone/" target="blank">Flower Zone!</a>
 
 		<a href="archive/monsters/" target="blank">Monster Adoption</a>
+
+
 
 	</section>
 </div>
