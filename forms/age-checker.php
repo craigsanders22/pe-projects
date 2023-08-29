d293c3bcfd6f0704834d9fa2dccba44740e8bddf
craigsanders22 <craig.sanders@me.com>
2023-08-28 23:24:44 -0400

    v

--- forms/age-checker.php
+++ forms/age-checker.php
@@ -4,15 +4,16 @@ body {
     margin: 0;
     padding: 0;
     background-color: #f7cfb6;
+
 }
 
 .container {
-    max-width: 400px;
+    max-width: 600px;
     margin: 100px auto;
     background-color: #ad858d;
     padding: 20px;
-    border-radius: 5px;
-    box-shadow: 0px 0px 10px rgba(1, 4, 4, 0.1);
+    border-radius: 8px;
+    box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
 }
 
 h1 {
@@ -29,6 +30,18 @@ h1 {
     color: white;
 }
 
+.container p {
+    font-family: optima;
+    font-size: 1.4rem;
+    color: white;
+    max-width: 35ch;
+    margin: auto;
+    padding-top: 20px;
+    text-align: center;
+
+}  
+
+
 form {
     text-align: center;
 }
@@ -38,26 +51,34 @@ label {
     margin-bottom: 10px;
 }
 
-input[type="submit"]
-{
-    font-size:64px;
-}
+
 
 input[type="number"] {
-    width: 100%;
+    width: 50%;
     padding: 10px;
-    border: 1px solid #6c6874;
+    border: 3px solid #6c6874;
     border-radius: 5px;
 }
 
 button {
-    padding: 10px 20px;
-    background-color: #a7868d;
+    padding: 10px 30px;
+    background-color: #6c6874;
+    font-family: optima;
+    font-size: 1.1rem;
     color: #fff;
-    border: none;
+    border: 3px solid #fff;
     border-radius: 5px;
     cursor: pointer;
 }
+
+  h4 {
+    font-family: optima;
+    font-size: 1.5rem;
+    color: white;
+    text-align: center;
+    
+  }
+
 
 
 </style>
@@ -77,27 +98,28 @@ button {
 <body>
     <div class="container">
         <h1>Legal Driving Age</h1>
+        <p>Just enter your current age and we'll let you know if you can legally drive a car.</p>
         <form action="age-checker.php" method="post">
-            <p><label for="age">What is your age?</label>
+            <p><label for="age">What is your age?</label></p>
             <input type="number" id="age" name="age" required>
             <p><button type="submit">Check</button></p>
 
         </form>
-    </p>
+ 
+   <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (isset($_POST['age'])) {
+                $age = intval($_POST['age']);
+                if ($age < 16) {
+                    echo "<h4>You are not old enough to legally drive...&nbsp;&nbsp;&#128534 </h4>";
+                } else {
+                    echo "<h4>You are old enough to drive!&nbsp;&nbsp; &#128512</h4>";
+                }
+            } else {
+                echo "<h4>Please provide your age.</h4>";
+            }
+        }
+        ?>
     </div>
 </body>
 </html>
-
-
- <?php
-        if (isset($_POST['age'])) {
-            $age = intval($_POST['age']);
-            if ($age < 16) {
-                echo "You are not old enough to legally drive.";
-            } else {
-                echo "You are old enough to drive.";
-            }
-        } else {
-            echo "Please provide your age.";
-        }
-        ?>
