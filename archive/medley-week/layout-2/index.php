821361603f2cb8ab4a6ec0d4b7bb72da3e28f3de
craigsanders22 <craig.sanders@me.com>
2023-08-29 11:24:32 -0400

    layout-2 setup

--- archive/medley-week/layout-2/index.php
+++ archive/medley-week/layout-2/index.php
@@ -6,13 +6,56 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
 	<link rel="stylesheet" href="../styles/site.css">
+	<link rel="stylesheet" href="layout-2.css">
 </head>
 
 <body>
 	<main class="adventure">
 		<div class="inner-column">
+			<section class="headline">
+				<picture>
+					<img src="joy.png" alt="">
+				</picture>
+				<h2 class="attention-voice"> Adventure travel done right</h2>
+				<p class="calm-voice">Wherever you want to go, whatever you want to get into, we've got a trip that’ll make your dream vacation come true. Visit like a local, explore at your own pace, and eat like a king (or a vegan king, if that's more your thing).</p>
+			</section>
 
-			<h1 class="loud-voice">TESTING</h1>
+			<section class="articles">
+				<div class="article-card">
+					<picture>
+						<img src="mountain.png" alt="">
+					</picture>
+					<h3 class="strong-voice">
+						The uncommon path
+					</h3>
+					<p class="calm-voice">We go where others don’t, exploring unique destinations and the lesser-seen sides of popular places.</p>
+				</div>
+
+					<div class="article-card">
+					<picture>
+						<img src="hike.png" alt="">
+					</picture>
+					<h3 class="strong-voice">
+						Expert, indespensable guides
+					</h3>
+					<p class="calm-voice">Our experienced, multitalented guides will keep you smiling, well-fed and learning new things around every bend.</p>
+				</div>
+
+					<div class="article-card">
+					<picture>
+						<img src="mountain.png" alt="">
+					</picture>
+					<h3 class="strong-voice">
+						Decades of experience
+					</h3>
+					<p class="calm-voice">For 30+ years, we’ve wowed travelers with exceptional service, flexible booking policies, and our dedication to travelers’ health & safety.</p>
+				</div>
+
+					
+
+
+
+
 
 
 
