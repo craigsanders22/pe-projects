3281e9fa301a3ce259e9f4204684f6d2fa5ba88d
craigsanders22 <craig.sanders@me.com>
2023-08-28 23:30:37 -0400

    v

--- forms/temp-converter.php
+++ forms/temp-converter.php
@@ -1,111 +1,103 @@
 <!DOCTYPE html>
 <html>
 <head>
+
 <style>
 
 body {
-    margin: 0;
-    padding: 0;
-    background-color: #f7cfb6;
-
+  margin: 0;
+  padding: 0;
+  background-color: #f7cfb6;
 }
 
 .container {
-    max-width: 600px;
-    margin: 100px auto;
-    background-color: #ad858d;
-    padding: 20px;
-    border-radius: 8px;
-    box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
+  max-width: 600px;
+  margin: 100px auto;
+  background-color: #ad858d;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
 }
 
 h1 {
-    font-family: palatino;
-    color: #fff;
-    font-size: 2.1rem;
-    text-align: center;
-    margin-bottom: 20px;
+  font-family: palatino;
+  color: #fff;
+  font-size: 2.1rem;
+  text-align: center;
+  margin-bottom: 20px;
 }
 
 .container form {
-    font-family: optima;
-    font-size: 1.4rem;
-    color: white;
+  font-family: optima;
+  font-size: 1.4rem;
+  color: white;
 }
 
 .container p {
-    font-family: optima;
-    font-size: 1.4rem;
-    color: white;
-    max-width: 35ch;
-    margin: auto;
-    padding-top: 20px;
-    text-align: center;
-
-}  
-
+  font-family: optima;
+  font-size: 1.4rem;
+  color: white;
+  max-width: 35ch;
+  margin: auto;
+  padding-top: 20px;
+  text-align: center;
+}
 
 form {
-    text-align: center;
+  text-align: center;
 }
 
 label {
-    display: block;
-    margin-bottom: 10px;
+  display: block;
+  margin-bottom: 10px;
 }
 
-
-
 input[type="number"] {
-    width: 50%;
-    padding: 10px;
-    border: 3px solid #6c6874;
-    border-radius: 5px;
+  width: 50%;
+  padding: 10px;
+  border: 3px solid #6c6874;
+  border-radius: 5px;
 }
 
 button {
-    padding: 10px 30px;
-    background-color: #6c6874;
-    font-family: optima;
-    font-size: 1.1rem;
-    color: #fff;
-    border: 3px solid #fff;
-    border-radius: 5px;
-    cursor: pointer;
+  padding: 10px 30px;
+  background-color: #6c6874;
+  font-family: optima;
+  font-size: 1.1rem;
+  color: #fff;
+  border: 3px solid #fff;
+  border-radius: 5px;
+  cursor: pointer;
 }
 
-  h4 {
-    font-family: optima;
-    font-size: 1.5rem;
-    color: white;
-    text-align: center;
-    
-  }
-
-
-
-</style>
+h4 {
+  font-family: optima;
+  font-size: 1.5rem;
+  color: white;
+  text-align: center;
+}
 
 
 
 
 </style>
 </head>
-
 <body>
 
-<div class="container">
-  <h2 class="attention-voice">Temperature Converter</h2>
-  <form method="post" action="">
-    <label for="temperature"><p class="strong-voice">Enter temperature:</label>
-    <input type="number" name="temperature" id="temperature" step="0.01" required><br><br>
-    
-    <label>Convert to:</label>
-    <input type="radio" name="unit" value="celsius" checked> Celsius
-    <input type="radio" name="unit" value="fahrenheit"> Fahrenheit<br><br>
-    
-    <input type="submit" name="convert" value="Convert">
-  </form>
+  <div class="container">
+    <h1 class="attention-voice">Temperature Converter</h1>
+    <form method="post" action="">
+      <label for="temperature">
+        <p class="strong-voice">Enter temperature:
+      </label></p>
+      <input type="number" name="temperature" id="temperature" step="0.01" required><br><br>
+
+      <label>Convert to:</label>
+      <input type="radio" name="unit" value="celsius" checked> Celsius
+      <input type="radio" name="unit" value="fahrenheit"> Fahrenheit<br><br>
+
+      <input type="submit" name="convert" value="Convert">
+    </form>
   
   <?php
   if (isset($_POST['convert'])) {
