45b9d0f953ed5094ca0bb81758d83747a83cd691
craigsanders22 <craig.sanders@me.com>
2023-08-30 17:05:30 -0400

    [ut up layouts

---
+++ projects/layout-garden/layout-2/index.php
@@ -0,0 +1,86 @@
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	
+	<link rel="stylesheet" href="layout-2.css">
+</head>
+
+<body>
+	<main class="adventure">
+		<div class="inner-column">
+			<section class="articles">
+				<ul>
+
+					<li>
+						<article>
+							<picture>
+								<img src="https://www.rei.com/assets/adventures/images/homepage/about/about-hero-tile/live.jpg" alt="">
+							</picture>
+							<div class="adventure-text">
+								<h3 class="attention-voice"> Adventure travel done right</h3>
+								<p class="calm-voice">Wherever you want to go, whatever you want to get into, we've got a trip that’ll make your dream vacation come true. Visit like a local, explore at your own pace, and eat like a king (or a vegan king, if that's more your thing).</p>
+							</div>
+						</article>
+					</li>
+
+					<li>
+						<article>
+							<picture>
+								<img src="https://www.rei.com/assets/adventures/images/homepage/about/about-uncommon-tile/live.jpg" alt="">
+							</picture>
+							<div class="adventure-text">
+								<h3 class="strong-voice">
+									The uncommon path
+								</h3>
+								<p class="calm-voice">We go where others don’t, exploring unique destinations and the lesser-seen sides of popular places.</p>
+							</div>
+						</article>
+					</li>
+
+					<li>
+						<article>
+							<picture>
+								<img src="https://www.rei.com/assets/adventures/images/homepage/about/about-guide-tile/live.jpg" alt="">
+							</picture>
+							<div class="adventure-text">
+								<h3 class="strong-voice">
+									Expert, indespensable guides
+								</h3>
+								<p class="calm-voice">Our experienced, multitalented guides will keep you smiling, well-fed and learning new things around every bend.</p>
+							</div>
+						</article>
+					</li>
+
+					<li>
+						<article>
+							<picture>
+								<img src="https://www.rei.com/assets/adventures/images/homepage/about/about-decades-tile/live.jpg" alt="">
+							</picture>
+							<div class="adventure-text">
+								<h3 class="strong-voice">
+									Decades of experience
+								</h3>
+								<p class="calm-voice">For 30+ years, we’ve wowed travelers with exceptional service, flexible booking policies, and our dedication to travelers’ health & safety.</p>
+							</div>
+						</article>
+					</li>
+
+				</ul>
+
+		</div>
+	</main>
+
+</body>
+
+</html>
+</html>
---
+++ projects/layout-garden/layout-3/index.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	
+	<link rel="stylesheet" href="layout-3.css">
+</head>
+
+
+<body>
+<section class="layout">
+	<inner-column>
+		<module>
+			<ul>
+				<li>
+					<picture>
+						<img src="https://www.rei.com/assets/adventures/images/promotion/uncruise/uncruise-whale-breah-2022/live.png" alt="#">
+					</picture>
+					<div class="info">
+						<h2 class="attention-voice">"Superb guides, they really made the trip!"</h2>
+						<p class="calm-voice">Small ships, cozy groups, boundless adventure. Set sail with UnCruise for an REI-style excursion of a lifetime. Cruise around Hawaii, the Galapagos Islands, Costa Rica and more.</p>
+						<a href="#">Get to know our guides</a>
+					</div>
+				</li>
+
+				<li>
+					<picture>
+						<img src="https://www.rei.com/adventures/assets/adventures/images/trip/gallery/northamerica/coa_09/live.jpg" alt="#">
+					</picture>
+					<div class="info">
+						<h2 class="attention-voice">Members save $250on UnCruise trips</h2>
+						<p class="calm-voice">Small ships, cozy groups, boundless adventure. Set sail with UnCruise for an REI-style excursion of a lifetime. Cruise around Hawaii, the Galapagos Islands, Costa Rica and more.</p>
+						<a href="#">Discover UnCruise</a>
+					</div>
+				</li>
+
+			</ul>
+		</module>
+	</inner-column>
+</section>
+</body>
+</html>
