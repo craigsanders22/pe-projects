f7d50ad05c410bbffd9e50e55b625863f079ee2d
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2023-09-05 11:41:35 -0400

    .

--- figma-code-challenge/footer.php
+++ figma-code-challenge/footer.php
@@ -1,11 +1,89 @@
 </inner-column>
-			</section>
+</section>
 
-		</main>
+</main>
 
-		<footer>
-			<h2>Zee footah</h2>
-		</footer>
-	</body>
+<footer>
+	<section class="footer-nav">
+		<ul>
+			<li>
+				<h3 class="strong-voice">
+					Product</h3>
+				<ul>
+					<li>
+						<a href="#" class="quiet-voice">Link Here</p>
+					</li>
+					<li>
+						<a href="#" class="quiet-voice">Link Here</p>
+					</li>
+
+					<li>
+						<a href="#" class="quiet-voice">Link Here</p>
+					</li>
+
+					<li>
+						<a href="#" class="quiet-voice">Link Here</p>
+					</li>
+
+					<li>
+						<a href="#" class="quiet-voice">Link Here</p>
+					</li>
+					<li>
+						<a href="#" class="quiet-voice">Link Here</p>
+					</li>
+
+				</ul>
+			</li>
+		</ul>
+		</li>
+
+		<li>
+			<h3 class="strong-voice">
+				Information</h3>
+			<ul>
+				<li>
+					<a href="#" class="quiet-voice">Link Here</p>
+				</li>
+				<li>
+					<a href="#" class="quiet-voice">Link Here</p>
+				</li>
+
+				<li>
+					<a href="#" class="quiet-voice">Link Here</p>
+				</li>
+
+			</ul>
+		</li>
+		</ul>
+		</li>
+
+		<li>
+			<h3 class="strong-voice">
+				Company</h3>
+			<ul>
+				<li>
+					<a href="#" class="quiet-voice">Link Here</p>
+				</li>
+				<li>
+					<a href="#" class="quiet-voice">Link Here</p>
+				</li>
+
+				<li>
+					<a href="#" class="quiet-voice">Link Here</p>
+				</li>
+
+				<li>
+					<a href="#" class="quiet-voice">Link Here</p>
+				</li>
+
+			</ul>
+		</li>
+		</ul>
+		</li>
+		</ul>
+	</section>
+
+</footer>
+</body>
 
 </html>
