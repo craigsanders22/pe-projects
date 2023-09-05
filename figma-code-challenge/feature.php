9d720641b13f5d56c6c3c06dac7d2a4e4faf3886
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2023-09-05 12:30:04 -0400

    .

---
+++ figma-code-challenge/feature.php
@@ -0,0 +1,63 @@
+<section class="feature-section">
+	
+
+	<div class="top-text">
+		<h3 class="strong-voice">Etiam Null Lectus Molestine At Vulputate.
+		</h3>
+		<p class="quiet-voice">Neque, Pulvinar Vestibulum Non Aliquam.
+		</p>
+	</div>
+
+	<div class="picture">
+		<img src="images/square.jpg" alt="">
+	</div>
+
+	<div class="feature-cards">
+		<ol>
+			<li>
+				<img src="https://peprojects.dev/images/dot.png" alt="">
+				<h4 class="strong-voice">Et, Odio Elementum</h4>
+				<p class="calm-voice">Aliquam Vehicula Pelletesque Id Mi QUam Ipsum. Arcu Nisi Faubicibus Mattis Etiam.
+				</p>
+			</li>
+
+			<li>
+				<img src="https://peprojects.dev/images/dot.png" alt="">
+				<h4 class="strong-voice">Et, Odio Elementum</h4>
+				<p class="calm-voice">Aliquam Vehicula Pelletesque Id Mi QUam Ipsum. Arcu Nisi Faubicibus Mattis Etiam.
+				</p>
+			</li>
+
+			<li>
+				<img src="https://peprojects.dev/images/dot.png" alt="">
+				<h4 class="strong-voice">Et, Odio Elementum</h4>
+				<p class="calm-voice">Aliquam Vehicula Pelletesque Id Mi QUam Ipsum. Arcu Nisi Faubicibus Mattis Etiam.
+				</p>
+			</li>
+
+			<li>
+				<img src="https://peprojects.dev/images/dot.png" alt="">
+				<h4 class="strong-voice">Et, Odio Elementum</h4>
+				<p class="calm-voice">Aliquam Vehicula Pelletesque Id Mi QUam Ipsum. Arcu Nisi Faubicibus Mattis Etiam.
+				</p>
+			</li>
+
+			<li>
+				<img src="https://peprojects.dev/images/dot.png" alt="">
+				<h4 class="strong-voice">Et, Odio Elementum</h4>
+				<p class="calm-voice">Aliquam Vehicula Pelletesque Id Mi QUam Ipsum. Arcu Nisi Faubicibus Mattis Etiam.
+				</p>
+			</li>
+
+			<li>
+				<img src="https://peprojects.dev/images/dot.png" alt="">
+				<h4 class="strong-voice">Et, Odio Elementum</h4>
+				<p class="calm-voice">Aliquam Vehicula Pelletesque Id Mi QUam Ipsum. Arcu Nisi Faubicibus Mattis Etiam.
+				</p>
+			</li>
+
+		</ol>
+
+	</div>
+
+</section>
