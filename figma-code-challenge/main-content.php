088dd9262e09962e8100e9d36e9fca691c827138
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2023-09-05 11:59:00 -0400

    html done through center content

--- figma-code-challenge/main-content.php
+++ figma-code-challenge/main-content.php
@@ -1,1 +1,20 @@
-main-content.php
+<section class="center-content">
+	<div class="center-text">
+		<p class="quiet-voice">
+			Rhoncus, Accumsam Dictum
+		</p>
+
+		<h2 class="attention-voice">
+			Etiam Nulla Lfulputate.
+		</h2>
+
+		<p class="quiet-voice">
+			Neque, Pulvinar Vestibulum Non Aliquam.
+		</p>
+
+	<div class="video-player">
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/8O_ifyIIrN4?si=19oB5qVFajdtdeRp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+	</div>
+
+	</div>
+</section>
