127cc42b689c5a61e26f6b8aa734142f61b60871
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2023-09-05 08:25:02 -0400

    figma to code initial commit

---
+++ figma-code-challenge/footer.php
@@ -0,0 +1,11 @@
+</inner-column>
+			</section>
+
+		</main>
+
+		<footer>
+			<h2>Zee footah</h2>
+		</footer>
+	</body>
+
+</html>
---
+++ figma-code-challenge/header.php
@@ -0,0 +1,26 @@
+
+<!doctype html>
+
+<html lang='en' class='special-magic no-js'>
+
+	<head>
+		<meta charset='utf-8'>
+		<meta name='viewport' content='width=device-width, initial-scale=1'>
+
+		<title>Figma to Code Sprint!</title>
+		<meta name='description' content=' logoipsum'>
+		<meta property='og:image' content='$todo'>
+
+		<link rel='stylesheet' href='styles/site.css'>
+	</head>
+
+
+	<body>
+		<header>
+			<h1 class='loud-voice'>logoipsum </h1>
+		</header>
+
+		<main>
+
+			<section class='section-name' id='and-its-id'>
+			<inner-column>
