e1c9b625c5431b600451bf49e5ad344d823c2a2d
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2023-09-05 22:50:17 -0400

    feature section done

--- figma-code-challenge/feature.php
+++ figma-code-challenge/feature.php
@@ -14,47 +14,25 @@
 
 	<div class="feature-cards">
 		<ol>
-			<li>
-				<img src="https://peprojects.dev/images/dot.png" alt="">
-				<h4 class="strong-voice">Et, Odio Elementum</h4>
-				<p class="calm-voice">Aliquam Vehicula Pelletesque Id Mi QUam Ipsum. Arcu Nisi Faubicibus Mattis Etiam.
-				</p>
-			</li>
 
-			<li>
-				<img src="https://peprojects.dev/images/dot.png" alt="">
-				<h4 class="strong-voice">Et, Odio Elementum</h4>
-				<p class="calm-voice">Aliquam Vehicula Pelletesque Id Mi QUam Ipsum. Arcu Nisi Faubicibus Mattis Etiam.
-				</p>
-			</li>
+		<?php 
+			$items = [1, 2, 3, 4, 5, 6]; 
 
-			<li>
-				<img src="https://peprojects.dev/images/dot.png" alt="">
-				<h4 class="strong-voice">Et, Odio Elementum</h4>
-				<p class="calm-voice">Aliquam Vehicula Pelletesque Id Mi QUam Ipsum. Arcu Nisi Faubicibus Mattis Etiam.
-				</p>
-			</li>
+			foreach ($items as $x) { 
+			?>
+				<li>
+					<picture>
+					<img src="https://peprojects.dev/images/dot.png" alt=""></picture>
+					<div class="card-text">
+					<h4 class="strong-voice">Et, Odio Elementum</h4>
+					<p class="calm-voice">Aliquam Vehicula Pelletesque Id Mi QUam Ipsum. Arcu Nisi Faubicibus Mattis Etiam.
+					</p>
+					</div>
+				</li>
+			<?php 
+			}
+			?>
 
-			<li>
-				<img src="https://peprojects.dev/images/dot.png" alt="">
-				<h4 class="strong-voice">Et, Odio Elementum</h4>
-				<p class="calm-voice">Aliquam Vehicula Pelletesque Id Mi QUam Ipsum. Arcu Nisi Faubicibus Mattis Etiam.
-				</p>
-			</li>
-
-			<li>
-				<img src="https://peprojects.dev/images/dot.png" alt="">
-				<h4 class="strong-voice">Et, Odio Elementum</h4>
-				<p class="calm-voice">Aliquam Vehicula Pelletesque Id Mi QUam Ipsum. Arcu Nisi Faubicibus Mattis Etiam.
-				</p>
-			</li>
-
-			<li>
-				<img src="https://peprojects.dev/images/dot.png" alt="">
-				<h4 class="strong-voice">Et, Odio Elementum</h4>
-				<p class="calm-voice">Aliquam Vehicula Pelletesque Id Mi QUam Ipsum. Arcu Nisi Faubicibus Mattis Etiam.
-				</p>
-			</li>
 		</ol>
 	</div>
 </section>
