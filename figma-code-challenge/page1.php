10b71cb7feee64416f0f9e1293ea760ad676b416
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2023-09-07 14:14:09 -0400

    3 page php initial

---
+++ figma-code-challenge/page1.php
@@ -0,0 +1,18 @@
+
+
+<?php 
+	$sections=['hero', 'main-content', 'feature', 'clients', 'cta', 'faq' ]
+?>
+
+<?php include('header.php') ?>
+
+<?php 
+	foreach ($sections as $section) {
+		include ($section . '.php');
+	} 
+ ?>
+
+
+
+
+<?php include('footer.php') ?>
