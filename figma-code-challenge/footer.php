514a49123d1feafcfe5ce7655fa7bdbdba53ec92
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2023-09-07 11:56:23 -0400

    .

--- figma-code-challenge/footer.php
+++ figma-code-challenge/footer.php
@@ -79,16 +79,16 @@
 					Subscribe</h3>
 
 				<div class="form-container">
-					<input type="email" class="email-input" placeholder="Email Address">
-					<button type="submit" class="submit-button">
-						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+					<input type="email" class="quiet-voice" placeholder="Email Address">
+					<button type="submit" class="submit-button">>
+						<!--<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
 							<path d="M0 0h24v24H0z" fill="none" />
 							<path fill="white" d="M21.2 0H2.8C1.2 0 0 1.2 0 2.8v18.4C0 22.8 1.2 24 2.8 24h18.4c1.6 0 2.8-1.2 2.8-2.8V2.8C24 1.2 22.8 0 21.2 0zm-7.2 14L2 4h20l-12 10z" />
-						</svg>
+						</svg>-->
 					</button>
 				</div>
 
-				<p class="quiet-voice">Lorem ipsum dolor sit amet consectetur adipisicing, elit. Ea facere beatae voluptas non eaque, recusandae nulla ullam, amet praesentium natus.</p>
+				<p class="quiet-voice">Lorem ipsum dolor sit amet consectetur adipisicing, elit. Ea facere beatae voluptas non eaque.</p>
 
 				</div>
 			</div>
