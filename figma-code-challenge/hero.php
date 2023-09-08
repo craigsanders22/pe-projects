54bbbf360c654d5fc75a71284f51bc39bbcad366
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2023-09-08 09:09:37 -0400

    modified for 3 templates

--- figma-code-challenge/hero.php
+++ figma-code-challenge/hero.php
@@ -11,6 +11,7 @@
 		<p class="calm-voice">Luctus felis sit lectus tristique diam ornare bibendum. Arcu auctor suspendisse luctus amet bibendum pellentesque lorem. Malesuada lobortis tristique tortor,
 		</p>	
 	</section>
+	<?php if($page == 1): ?>
 	<section class="hero-buttons">
 		
 		<button class="get-started">
@@ -20,6 +21,28 @@
 		<button class="trial-button">
 		<a href="#">Start Free Trial</a>
 		</button>
+	</section>
+	<?php endif; ?>
+
+	<?php if($page == 2): ?>
+	<section class="hero-buttons">
+		
+		<button class="get-started">
+		<a href="#">Get Started</a>
+		</button>
+
+		<?php endif; ?>
+
+
+
+	<?php if($page == 3): ?>
+		
+<div class="form-container">
+					<input type="email" class="quiet-voice" placeholder="Email Address">
+					<button>SignUp</button>
+				</div>
+
+		<?php endif; ?>
 	</div>
 </inner-column>
 </section>
