51fad330c88a7c577d1f39edc80bce224f6439b9
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2023-09-08 09:46:42 -0400

    modified for 3 templates

--- figma-code-challenge/CTA.php
+++ figma-code-challenge/CTA.php
@@ -1,8 +1,8 @@
 <section class="call-to-action">
 <inner-column>
-	
+	<?php if(in_array($page, [1, 2])): ?>
 	<section class="intro-area">
-		<p class="calm-voice">Lorem, ipsum, dolor.
+		<p class="quiet-voice">Lorem, ipsum, dolor Dictum.
 		</p>	
 	</section>
 	<section class="cta-headline">
@@ -15,10 +15,55 @@
 		<button class="get-started">
 		<a href="#">Get Free Quotes</a>
 		</button>
-
+		<?php endif ; ?>
+<?php if($page == 2): ?>
 		<button class="trial-button">
 		<a href="#">Free SignUp</a>
 		</button>
+	<?php endif ; ?>
+
+
+
+
+
+
+
+
+<?php if($page == 3): ?>
+<section class= "page3-CTA">
+<section class="cta-headline">
+	<div class="cta-text">
+		<h1 class="loud-voice">Lorem ipsum dolor sit amet 
+		</h1>
 	</section>
+
+	<section class="intro-area">
+		<p class="quiet-voice">Lorem, ipsum, dolor Dictum.
+		</p>	
+	</section>
+</div>
+
+	<div class="form-container">
+					<input type="email" class="quiet-voice" placeholder="Email Address">
+					<button>SignUp</button>
+				</div>
+			</div>
+	<div>
+		</section>
+	<img src="images/square.jpg" alt="">
+	</div>
+<?php endif ; ?>
+	</section>
+
+</section>
+
+
+
+
+
+
+
+
+
 </inner-column>
 </section>
