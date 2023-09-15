9ad431e2636f7a6db71b781097cd5e2f2276636d
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2023-09-15 10:43:59 -0400

    resume update - education section

--- resume/index.php
+++ resume/index.php
@@ -1,111 +1,155 @@
 <?php include('../header.php');?>
 
-
-
 <main class="resume">
-     <section class="resume-intro">
-  <div class="inner-column">
-    
-  	<div>
-      <h1 class="loud-voice">Craig Sanders</h1>
-      <p class="calm-voice">Indianapolis, IN 46219 | craig.sanders@me.com | +1 765 721 0820</p>
-    </div>
-</div>
-    </section>
+	<section class="resume-intro">
+		<div class="inner-column">
 
+			<div>
+				<h1 class="loud-voice">Craig Sanders</h1>
+				<p class="calm-voice">Indianapolis, IN 46219 | craig.sanders@me.com | 765 721 0820</p>
+			</div>
+		</div>
+	</section>
 
-    <section class="work-experience">
-       <div class="inner-column">
-      
+	<section class="work-experience">
+		<div class="inner-column">
 
-      <h2 class="attention-voice">Work Experience</h2>
-      <div class="work-details">
-        
-        	<div class="top-details">
+			<h2 class="loud-voice">Work Experience</h2>
+			<div class="work-details">
 
-        		<div class="title-image">
-        			<img src="../assets/cart.png" alt=""></div>
+				<div class="top-details">
 
-        		<div class="title-text">
-      	  <h3 class="attention-voice">Wildwood Market - Indianapolis, IN</h3>
-      
-      
-        <h3 class="work-title attention-voice ">Small Business Owner / Operator</h3>
-        
+					<div class="title-text">
+						<h3 class="company-name attention-voice">Wildwood Market</h3>
+						<h3 class="location strong-voice">
+							Indianapolis, IN</h3>
 
- 	</div>
-      </div>
-      
+						<div class="title-image">
+							<img src="../assets/cart.png" alt="">
+							<h3 class="work-title attention-voice ">Small Business Owner / Operator</h3>
+						</div>
 
+					</div>
+				</div>
 
+				<div class="work-dates strong-voice">September 2014 - April 2023</div>
 
+				<div class="role-text">
+					<ul>
+						<li class="
+         calm-voice">Established and managed a successful family-owned specialty market, supporting local farmers and food brands.</li>
+						<li>Curated a thoughtful selection of fresh and packaged food products in the Fountain Square neighborhood.</li>
 
+					</ul>
+				</div>
+			</div>
 
-        <div class="work-dates strong-voice">September 2014 - Present</div>
-        
-        <ul>
-          <li class="calm-voice">Established and managed a successful family-owned specialty market, supporting local farmers and food brands.</li>
-          <li>Curated a thoughtful selection of fresh and packaged food products in the Fountain Square neighborhood.</li>
+			<div class="work-details">
+				<div class="top-details">
 
-        </ul>
-      </div>
-     
- 
+					<div class="title-text">
+						<h3 class="company-name attention-voice">Traders Point Creamery</h3>
 
-      
-      <!-- <div class="work-details"> 
-        <div class="work-title strong-voice">Director of Ops</div>
-        <div>Traders Point Creamery - Zionsville, IN</div>
-        <div class="work-dates"> August 2010- September 2014</div>
-        <ul>
-          <li>Transformed a local organic dairy brand into a nationally distributed product available in grocery and specialty chains.</li>
-          <li>Cultivated and maintained relationships with national distributors, leading to increased market presence.</li>
-        </ul>
-      </div>
+						<h3 class="location strong-voice">
+							Zionsville, IN</h3>
 
-      <div class="work-details">
-        <div class="work-title strong-voice">Director of Sales and Business Development</div>
-        <div>Traders Point Creamery - Zionsville, IN</div>
-        <div class="work-dates">June 2007 - July 2010</div>
-        <ul>
-          <li>Transformed a local organic dairy brand into a nationally distributed product available in grocery and specialty chains.</li>
-          <li>Cultivated and maintained relationships with national distributors, leading to increased market presence.</li>
-        </ul>
-      </div>
-    </div>
-    </section>
+						<div class="title-image">
+							<img src="../assets/networking.png" alt="">
+							<h3 class="work-title attention-voice">Director of Operations</h3>
+						</div>
 
-    
+					</div>
+				</div>
 
--->
+				<div class="work-dates strong-voice">October 2010 - September 2014</div>
 
-    <section class="education">
-       <div class="inner-column">
-      <h2 class="attention-voice">Education</h2>
-      <div class="education-details">
-        <div>Indiana University-Purdue University Indianapolis - Indianapolis, IN</div>
-        <div>English</div>
-        <div>August 2003 - May 2006</div>
-      </div>
-    </div>
-    </section>
+				<div class="role-text">
+					<ul>
+						<li class="
+         calm-voice">Transformed a local organic dairy brand into a nationally distributed product available in grocery and specialty chains.</li>
 
-    
+						<li>Cultivated and maintained relationships with national distributors, leading to increased market presence.</li>
+					</ul>
+				</div>
 
+				<div class="work-details">
+					<div class="top-details">
 
-    <section class="skills">
- <div class="inner-column">
-      <h2 class="attention-voice">Skills</h2>
-      <ul class="skills-list">
-        <li class="skills-item">Relationship Building and Sales</li>
-        <li class="skills-item">Team Leadership and Management</li>
-        <li class="skills-item">Budgeting and Forecasting</li>
-        <li class="skills-item">Sales and Marketing Strategy</li>
-      </ul>
-    </div>
-    </section>
-  </div>
+						<div class="title-image">
+							<img src="../assets/growth.png" alt="">
+							<h3 class="work-title attention-voice">Key Account Sales Manager</h3>
+						</div>
 
+					</div>
+					</h3>
+
+					<div class="work-dates strong-voice">June 2007 - August 2010</div>
+
+					<div class="role-text">
+						<ul>
+							<li class="
+         calm-voice">Transformed a local organic dairy brand into a nationally distributed product available in grocery and specialty chains.</li>
+							<li>Cultivated and maintained relationships with national distributors, leading to increased market presence.</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			</h3>
+		</div>
+		</div>
+		</div>
+		</div>
+
+	</section>
+
+	<section class="education">
+		<div class="inner-column">
+
+			<h2 class="loud-voice">Education</h2>
+
+			<div class="education-intro">
+				<div class="title-image">
+					<img src="../assets/rocket.png" alt="">
+				</div>
+
+				<div class="attention-voice school">Perpetual Education - Design for the Web
+				</div>
+			</div>
+
+			<div class="education-text">
+				<div class="strong-voice degree">Web Design Mentorship, 2023</div>
+
+			</div>
+
+			<div class="education-intro">
+				<div class="title-image">
+					<img src="../assets/exam.png" alt="">
+				</div>
+
+				<div class="attention-voice school">Indiana University - Indianapolis
+				</div>
+			</div>
+
+			<div class="education-text">
+				<div class="strong-voice degree">Bachelor of Arts in English, 2007</div>
+
+			</div>
+
+		</div>
+	</section>
+
+	<section class="skills">
+		<div class="inner-column">
+			<h2 class="attention-voice">Skills</h2>
+			<ul class="skills-list">
+				<li class="skills-item">Relationship Building and Sales</li>
+				<li class="skills-item">Team Leadership and Management</li>
+				<li class="skills-item">Budgeting and Forecasting</li>
+				<li class="skills-item">Sales and Marketing Strategy</li>
+			</ul>
+		</div>
+	</section>
+	</div>
 
 </main>
 
