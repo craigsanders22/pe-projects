3f15fb9fe8299cc92755529cb808a110cc705502
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2023-09-18 14:16:34 -0400

    ddfdf

--- forms/character-counter.js
+++ forms/character-counter.js
@@ -1,27 +1,19 @@
-// console.clear();
-
-// const $form = document.querySelector('form');
-// const $form = $form.querySelector('input');
-// const $outlet = document.querySelector('output');
-
-// $form.addEventListener('submit', function(event) {
-// 	event.preventDefault();
-// })
-
-
 
 
 
 
 function countCharacters() {
-            // Get the input value
-            var inputString = document.getElementById("inputString").value;
+    // Get the input value
+    var inputString = document.getElementById("inputString").value;
+    
+    // Count the characters
+    var charCount = inputString.length;
 
-            // Count the characters
-            var charCount = inputString.length;
+	// Output the result
+	document.getElementById("result").innerHTML = '"' + inputString + '" has ' + charCount + ' characters.';
+    
+	    return false;
 
-            // Output the result
-            document.getElementById("result").innerHTML = '"' + inputString + '" has ' + charCount + ' characters.';
         }
 
 
