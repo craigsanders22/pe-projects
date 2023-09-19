03462ee12f3ec0ef674afecd2151ef5eca0e029d
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2023-09-19 11:23:21 -0400

    fixed error message

--- forms/retirement-age.js
+++ forms/retirement-age.js
@@ -3,18 +3,18 @@ function calculateRetirement() {
     var currentAge = currentAgeInput.value;
     var retireAge = retireAgeInput.value;
 
-    // if (currentAge >= retireAge) {
-    //     alert("Invalid input. You must retire after your current age.");
-    //     return;
-    // }
+    if (currentAge > retireAge) {
+        alert("Invalid input. You must retire after your current age.");
+        return;
+    }
 
     var yearsToRetire = retireAge - currentAge;
     var currentYear = new Date().getFullYear();
     var retirementYear = currentYear + yearsToRetire;
 
     var output = "You have " + yearsToRetire + " years until you can retire. ";
-    output += "It is currently " + currentYear + ". ";
-    output += "You can retire in " + retirementYear + ".";
+
+    output += "It is currently " + currentYear + " so you can retire in " + retirementYear + "!";
 
     outputElement.innerHTML = output;
 }
