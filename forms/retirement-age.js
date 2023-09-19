7fc2302601abbc330a1ce0b73d5e155b03f102ae
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2023-09-19 11:53:53 -0400

    fixed output?

--- forms/retirement-age.js
+++ forms/retirement-age.js
@@ -1,23 +1,33 @@
+
+// pull in function
 function retirementAge(event) {
     
     event.preventDefault();
 
+    // assign variables from form values
     var outputElement = document.getElementById("output");
     var currentAge = currentAgeInput.value;
     var retireAge = retireAgeInput.value;
 
+
+    // error handling
     // if (currentAge <= retireAge) {
     //     alert("Invalid input. You must retire after your current age.");
     //     return;
     // }
 
+    // build output variables
     var yearsToRetire = retireAge - currentAge;
     var currentYear = new Date().getFullYear();
     var retirementYear = currentYear + yearsToRetire;
 
+
+    // build output
     var output = "You have " + yearsToRetire + " years until you can retire. ";
 
     output += "It is currently " + currentYear + " so you can retire in " + retirementYear + "!";
 
-    outputElement.innerHTML = output;
+
+    // run output
+    document.getElementById("output").innerHTML = output;
 }
