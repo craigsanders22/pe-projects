e799dcab9e01313526ac46d2639f59af69aa0b81
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2023-09-19 09:24:43 -0400

    saying hello initial

---
+++ forms/saying-hello.js
@@ -0,0 +1,13 @@
+function sayHello(event) {
+           
+            event.preventDefault();
+
+
+            // Get the input name
+            var inputString = document.getElementById("inputString").value;
+
+        
+
+            // Output the result
+            document.getElementById("result").innerHTML = 'Hello ' + inputString + ', nice to e-meet you!';
+        }
---
+++ forms/saying-hello.php
@@ -0,0 +1,18 @@
+<?php include 'form-header.php' ?>
+
+
+
+ <script src="saying-hello.js"></script>
+
+
+
+<body>
+
+	   <h1>Hello!</h1>
+    <form onsubmit="sayHello(event)">
+        <label for="inputString">What is your name?</label>
+        <input type="text" id="inputString" required>
+        <br>
+        <input type="submit" value="Let's Meet">
+    </form>
+    <h4 id="result"></h4>
