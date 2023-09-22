841e8da1d52725ad6badaba33db8d9190c2cbbf5
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2023-09-22 09:23:36 -0400

    .

--- forms/guessing-game.js
+++ forms/guessing-game.js
@@ -4,57 +4,60 @@ var outputElement = document.querySelector('output');
 
 // start the game
 function startGame() {
-  const difficulty = parseInt(document.querySelector("#difficulty").value);
+	const difficulty = parseInt(document.querySelector("#difficulty").value);
 
-  if (difficulty === 1) {
-    maxRange = 10;
-  } else if (difficulty === 2) {
-    maxRange = 100;
-  } else if (difficulty === 3) {
-    maxRange = 1000;
-  }
+	if (difficulty === 1) {
+		maxRange = 10;
+	} else if (difficulty === 2) {
+		maxRange = 100;
+	} else if (difficulty === 3) {
+		maxRange = 1000;
+	}
 
   //generate secret number
-  secretNumber = Math.floor(Math.random() * maxRange) + 1;
-  attempts = 0;
+	secretNumber = Math.floor(Math.random() * maxRange) + 1;
+	attempts = 0;
 
   //prompt for guess
-  document.querySelector("#instructions").textContent = `I have my number. It's between 1 and ${maxRange}.`;
+	document.querySelector("#instructions").textContent = `I have my number. It's between 1 and ${maxRange}.`;
 }
 
 //submit guess for comparison 
 function submitGuess() {
-  const guess = parseInt(document.querySelector("#guess").value);
+	const guess = parseInt(document.querySelector("#guess").value);
 
-   document.querySelector("#result").textContent = "";
+	document.querySelector("#result").textContent = "";
 
-  if (isNaN(guess) || guess < 1 || guess > maxRange) {
-    document.querySelector("#result").textContent = "Please enter a valid number within the range.";
-    return;
-  }
+	if (isNaN(guess) || guess < 1 || guess > maxRange) {
+		document.querySelector("#result").textContent = "Please enter a valid number within the range.";
+		return;
+	}
 
-  attempts++;
-  
-  renderMessage(guess);
+  //count attempts
+	attempts++;
+
+	renderMessage(guess);
 }
 
+//creat message
 function createMessage(guess) {
-  if (guess === secretNumber) {
-    return `You got it in ${attempts} guesses!`;
-  } else if (guess < secretNumber) {
-    return `Too low. Guess again.`;
-  } else {
-    return `Too high. Guess again.`;
-  }
+	if (guess === secretNumber) {
+		return `You got it in ${attempts} guesses!`;
+	} else if (guess < secretNumber) {
+		return `Too low. Guess again.`;
+	} else {
+		return `Too high. Guess again.`;
+	}
 }
+
 
 function renderMessage(guess) {
-  outputElement.innerHTML = createMessage(guess);
+	outputElement.innerHTML = createMessage(guess);
 }
 
 document.querySelector('form').addEventListener("submit", function(event) {
-  event.preventDefault();
-  submitGuess();
+	event.preventDefault();
+	submitGuess();
 });
 
 
--- forms/guessing-game.php
+++ forms/guessing-game.php
@@ -2,36 +2,36 @@
 
 <body>
 	<div class="inner-column">
-<div class="container">
-<h1>Guess the Number</h1>
+		<div class="container">
+			<h1>Guess the Number</h1>
 
-<div id="game-container">
-  <form onsubmit="startGame(); return false;">
-    <label for="difficulty">Choose a difficulty level:</label>
-    <select id="difficulty">
-      <option value="1">1 - Easy (1-10)</option>
-      <option value="2">2 - Medium (1-100)</option>
-      <option value="3">3 - Hard (1-1000)</option>
-    </select>
-    <button type="submit">Start Game</button>
-  </form>
-</div>
+			<div id="game-container">
+				<form onsubmit="startGame(); return false;">
+					<label for="difficulty">Choose a difficulty level:</label>
+					<select id="difficulty">
+						<option value="1">1 - Easy (1-10)</option>
+						<option value="2">2 - Medium (1-100)</option>
+						<option value="3">3 - Hard (1-1000)</option>
+					</select>
+					<button type="submit">Start Game</button>
+				</form>
+			</div>
 
-<div id="game">
-  <p id="instructions"></p>
-  <form onsubmit="submitGuess(); return false;">
-    <input type="text" id="guess" placeholder="Enter your guess">
-    <button type="submit">Submit Guess</button>
-  </form>
+			<div id="game">
+				<p id="instructions"></p>
+				<form onsubmit="submitGuess(); return false;">
+					<input type="text" id="guess" placeholder="Enter your guess">
+					<button type="submit">Submit Guess</button>
+				</form>
 
-  <h4><output></output></h4>
-  <p id="result"></p>
-  <p id="attempts"></p>
-</div>
+				<h4><output></output></h4>
+				<p id="result"></p>
+				<p id="attempts"></p>
+			</div>
 
 
-</div>
+		</div>
 
-</div>
+	</div>
 </body>
 <script src="guessing-game.js"></script>
