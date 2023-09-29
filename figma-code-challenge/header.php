4d882b2268d4913ec47adebd198abe85f2921439
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2023-09-29 15:16:43 -0400

    .

--- figma-code-challenge/header.php
+++ figma-code-challenge/header.php
@@ -20,6 +20,7 @@
         echo '<link rel="stylesheet" href="' . $cssFile . '">';
 		?>
 		<link rel='stylesheet' href='styles/site.css'>
+		<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 	</head>
 	<body>
 		<header class="site-header">
