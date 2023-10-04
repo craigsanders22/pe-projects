218ec624b1dbac10f33472222a9d27757a97c570
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2023-10-04 06:08:56 -0400

    music app encapsulation

--- projects/todo/musicapp.js
+++ projects/todo/musicapp.js
@@ -1,48 +1,58 @@
-
-
-function update(note = "") {
+const playlistApp = {
+  
+  playlist: [],
+  count: 0,
+  
+  update: function(note = "") {
     console.log(`-------- ${note}`);
-    console.log('playlist:', playlist);
-}
+    console.log('playlist:', this.playlist);
+  },
 
-const playlist = [];
-var count = 0;
-
-function add(songTitle, songArtist) {
+  add: function(songTitle, songArtist) {
     const song = {
-        id: count++,
-        title: songTitle,
-        artist: songArtist
+      id: this.count++,
+      title: songTitle,
+      artist: songArtist
     };
-    playlist.push(song);
-    update(`Added ${songTitle} by ${songArtist}`);
-}
+    this.playlist = [...this.playlist, song];
+   // .push(song);
+    this.update(`Added ${songTitle} by ${songArtist}`);
+  },
 
-function remove(id) {
-        var song = playlist[id];
-        update(`Removed ${song.title} by ${song.artist}`);
-        playlist.splice(id, 1);
-    };
+  remove: function(id) {
+    var song = this.playlist[id];
+    if(song) {
+      this.update(`Removed ${song.title} by ${song.artist}`);
+      this.playlist.splice(id, 1);
+    }
+  },
 
-function complete(id) {
-	var song = playlist[id];
-	if(song) {
-		song.complete = true;
-		update(`Played ${song.title} by ${song.artist}`);
-	};
-}
+  complete: function(id) {
+    var song = this.playlist[id];
+    if(song) {
+      song.complete = true;
+      this.update(`Played ${song.title} by ${song.artist}`);
+    }
+  },
+  
+  updateSong: function(id, newTitle, newArtist) {
+    var song = this.playlist[id];
+    if(song) {
+      song.title = newTitle;
+      song.artist = newArtist;
+      this.update(`Updated song with the ID of ${id}: new title is '${newTitle}', new artist is '${newArtist}'`);
+    }
+  },
+  
+};
+
+console.log( playlistApp.update("Hello") );
 
 
+playlistApp.add('Counting Cars', 'Snow Patrol');
+playlistApp.add('Backyard', 'Guster');
+playlistApp.add('Counting Cars', 'Snow Patrol');
+playlistApp.complete(1);
+playlistApp.remove(2);
 
 
-add('Viva La Vida', 'Coldplay');
-add('Yesterday', 'The Beatles');
-add('Yellow', 'Coldplay');
-add('Come Together', 'The Beatles');
-add('The Scientist', 'Coldplay');
-add('Something', 'The Beatles');
-add('Paradise', 'Coldplay');
-add('Hey Jude', 'The Beatles');
-complete (1);
-remove (3);
-add('Come Together', 'The Beatles');
