b99d93686683a0803625085cfdcc3f9916fc694e
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2023-10-06 23:41:00 -0400

    todo week

--- projects/todo/app.js
+++ projects/todo/app.js
@@ -1,39 +1,123 @@
-function update(note = "") {
-console.log(`-------- ${note}`);
-console.log('todos:',todos);
-}
-
-const todos = [];
-var count = 0;
+var todos = [];
+var lastId = 0;
+const $form = document.querySelector('form');
+const $input = document.querySelector('input');
+const $output = document.querySelector('output');
 
 function add(content) {
 	const todo = {
-		id: count++,
-		content: content
+		id: `a-${lastId++}`,
+		content,
+		complete: false,
+
 	};
-	todos.push(todo);
-	update(`Added ${content}`);
+
+	todos = [...todos, todo];
+	renderTodos(todos);
+
 }
 
 function remove(id) {
-	var todo = {};
-	update(`Removed ${todos[id].content}`);
-	todos.splice(id, 1);
+	const filtered = todos.filter( function(todo) {
+		return todo.id != id;
+	});
+
+	todos = [...filtered];
 }
 
-function complete(id) {
-	todos[id].complete = true;
-	update(`Completed ${todos[id].content}`);
+
+function complete (id) {
+	for (let i = 0; i < todos.length; i++) {
+		if ( todos[i].id == id ) {
+			todos[i].complete = true;
+		}
+	}
+}
+
+function renderTodo(todo) {
+	return `
+		<li data-id='${todo.id}'>
+		<todo-card>
+			<h2>${todo.content}</h2>
+			<actions>
+			<button>remove</button>
+
+			</actions>
+		</todo-card>
+		</li>
+	`;
 
 }
 
-add('finish class work');
-add('pick up olivia');
-add('go to Skyzone');
-add('go to Trader Joes');
+function renderTodos(todos) {
+	var template = "<ul>";
+	todos.forEach( function(todo) {
+		template += renderTodo(todo);
+		});
+	template += "</ul>";
+	$output.innerHTML = template;
+}
 
-remove (2);
-complete (1);
+add("Do the dishes");
 
-add('eat lunch');
+$form.addEventListener('submit', function(event) {
+	event.preventDefault();
+	add( $input.value );
+	$input.value = "";
+	console.log('todos: ', todos);
+
+});
+
+$output.addEventListener('click', function(event) {
+
+	if(event.target.textContent == 'remove') {
+		const id = event.target.closest('li').dataset.id;
+		remove(id);
+	}
+
+});
+
+
+
+
+
+
+// function update(note = "") {
+// console.log(`-------- ${note}`);
+// console.log('todos:',todos);
+// }
+
+// const todos = [];
+// var count = 0;
+
+// function add(content) {
+// 	const todo = {
+// 		id: count++,
+// 		content: content
+// 	};
+// 	todos.push(todo);
+// 	update(`Added ${content}`);
+// }
+
+// function remove(id) {
+// 	var todo = {};
+// 	update(`Removed ${todos[id].content}`);
+// 	todos.splice(id, 1);
+// }
+
+// function complete(id) {
+// 	todos[id].complete = true;
+// 	update(`Completed ${todos[id].content}`);
+
+// }
+
+// add('finish class work');
+// add('pick up olivia');
+// add('go to Skyzone');
+// add('go to Trader Joes');
+
+// remove (2);
+// complete (1);
+
+// add('eat lunch');
 
--- projects/todo/musicapp.js
+++ projects/todo/musicapp.js
@@ -1,55 +1,113 @@
 
+console.clear();
+
+localStorage.clear();
+
+
+
 
 function Playlist(title, creator) {
+    this.title = title;
+    this.creator = creator;
+    this.playlist = [];
+    // load existing/current playlist from localStorage 
+    var storedPlaylist = localStorage.getItem('playlist');
+    if (storedPlaylist) {
+        this.playlist = JSON.parse(storedPlaylist);
+    }
+    // id creator 
+    this.idIncrementor = this.playlist.length;
 
-	this.title = title,
-	this.creator = creator,
-	this.playlist = [];
-	this.idIncrementor = 0;
+    // save method used in all functions / available for parsing
+    this.save = function() {
+        localStorage.setItem('playlist', JSON.stringify(this.playlist));
+    }
 
-};
+    this.add = function(songTitle, songArtist) {
+        const song = {
+            id: `a${this.idIncrementor++}`,
+            title: songTitle,
+            artist: songArtist
+        };
+        this.playlist.push(song);
+        console.log(`Added ${songTitle} by ${songArtist}`);
+        this.save();
+    };
 
-Playlist.prototype.update = function(note = "") {
-	console.log(`-- ${note}`);
-	console.log('playlist:', this.playlist);
-};
+    this.remove = function(id) {
+        var song = this.playlist[id];
+        if(song) {
+            this.playlist.splice(id, 1);
+            console.log(`Removed ${song.title} by ${song.artist}`);
+            this.save();
+        }
+    };
 
-Playlist.prototype.add = function(songTitle, songArtist) {
-	const song = {
-		id: `a${this.idIncrementor++}`,
-		title: songTitle,
-		artist: songArtist
-	};
-	this.playlist = [...this.playlist, song];
-	this.update(`Added ${songTitle} by ${songArtist}`);
-};
+    this.complete = function(id) {
+        var song = this.playlist[id];
+        if(song) {
+            song.complete = true;
+            console.log(`Played ${song.title} by ${song.artist}`);
+            this.save();
+        }
+    };
 
-Playlist.prototype.remove = function(id) {
-	var song = this.playlist[id];
-	if(song) {
-		this.playlist.splice(id, 1);
-		this.update(`Removed ${song.title} by ${song.artist}`);
-	}
-};
-
-Playlist.prototype.complete = function(id) {
-	var song = this.playlist[id];
-	if(song) {
-		song.complete = true;
-		this.update(`Played ${song.title} by ${song.artist}`);
-	}
-};
-
-Playlist.prototype.updateSong = function(id, newTitle, newArtist) {
-	var song = this.playlist[id];
-	if(song) {
-		song.title = newTitle;
-		song.artist = newArtist;
-		this.update(`Updated song with the ID of ${id}: new title is '${newTitle}', new artist is '${newArtist}'`);
-	}
-};
+    this.updateSong = function(id, newTitle, newArtist) {
+        var song = this.playlist[id];
+        if(song) {
+            song.title = newTitle;
+            song.artist = newArtist;
+            console.log(`Updated song with the ID of ${id}: new title is '${newTitle}', new artist is '${newArtist}'`);
+            this.save();
+        }
+    };
+}
 
 
+
+
+// Playlist.prototype.add = function(songTitle, songArtist) {
+// 	const song = {
+// 		id: `a${this.idIncrementor++}`,
+// 		title: songTitle,
+// 		artist: songArtist
+// 	};
+// 	this.playlist = [...this.playlist, song];
+// 	this.update(`Added ${songTitle} by ${songArtist}`);
+// };
+
+// Playlist.prototype.remove = function(id) {
+// 	var song = this.playlist[id];
+// 	if(song) {
+// 		this.playlist.splice(id, 1);
+// 		this.update(`Removed ${song.title} by ${song.artist}`);
+// 	}
+// };
+
+// Playlist.prototype.complete = function(id) {
+// 	var song = this.playlist[id];
+// 	if(song) {
+// 		song.complete = true;
+// 		this.update(`Played ${song.title} by ${song.artist}`);
+// 	}
+// };
+
+// Playlist.prototype.updateSong = function(id, newTitle, newArtist) {
+// 	var song = this.playlist[id];
+// 	if(song) {
+// 		song.title = newTitle;
+// 		song.artist = newArtist;
+// 		this.update(`Updated song with the ID of ${id}: new title is '${newTitle}', new artist is '${newArtist}'`);
+// 	}
+// };
+
+
+
+
+
+
+
+// the data 
 const highSchool = new Playlist('High School', 'Craig');
 const college = new Playlist('College', 'Bob');
 const clubbing= new Playlist('Clubbing', 'Frank');
@@ -127,163 +185,5 @@ console.log(college.playlist);
 
 
 
-
-
-
-
-
-
-// // Add songs to playlist1
-// playlist1.add("Song Title 1", "Song Artist 1");
-// playlist1.add("Song Title 2", "Song Artist 2");
-
-// // Add songs to playlist2
-// playlist2.add("Song Title 3", "Song Artist 3");
-// playlist2.add("Song Title 4", "Song Artist 4");
-
-// // You can then access the different playlists and their songs
-// console.log(playlist1.playlist); // Prints the songs in playlist1
-// console.log(playlist2.playlist); // Prints the songs in playlist2
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function Playlist(title, creator) {
-	
-// 	this.title = title,
-// 	this.creator = creator,
-
-// 	this.roar = function() {
-// 		alert(`Now playing playlist: ${this.title}! Created by: ${this.creator}`);
-// 	}
-
-
-// }
-
-// const highSchool = new Playlist('High School', 'Craig');
-// const college = new Playlist('College', 'Bob');
-// const clubbing= new Playlist('Clubbing', 'Frank');
-// const wedding = new Playlist('Wedding', 'Joe');
-
-// console.log(wedding.creator);
-
-// college.roar();
-
-// Playlist.prototype.whisper = function() {
-//   console.log(`Hi I'm ${this.creator} and I created this playlist`)
-// };
-
-// wedding.whisper(); 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const playlistApp = {
-
-// 	playlist: [],
-// 	idIncrementor: 0,
-
-// 	update(note = "") {
-// 		console.log(`-------- ${note}`);
-// 		console.log('playlist:', this.playlist);
-		
-// 	},
-
-// 	add(songTitle, songArtist) {
-// 		const song = {
-// 			id: `a${this.idIncrementor++}`,
-// 			title: songTitle,
-// 			artist: songArtist
-// 		};
-// 		this.playlist = [...this.playlist, song];
-//    // replaces .push(song);
-// 		this.update(`Added ${songTitle} by ${songArtist}`);
-// 	},
-
-// 	remove(id) {
-// 		var song = this.playlist[id];
-// 		if(song) {
-// 			this.playlist.splice(id, 1);
-// 			this.update(`Removed ${song.title} by ${song.artist}`);
-// 		}
-// 	},
-
-// 	complete(id) {
-// 		var song = this.playlist[id];
-// 		if(song) {
-// 			song.complete = true;
-// 			this.update(`Played ${song.title} by ${song.artist}`);
-// 		}
-// 	},
-
-// 	updateSong(id, newTitle, newArtist) {
-// 		var song = this.playlist[id];
-// 		if(song) {
-// 			song.title = newTitle;
-// 			song.artist = newArtist;
-// 			this.update(`Updated song with the ID of ${id}: new title is '${newTitle}', new artist is '${newArtist}'`);
-// 		}
-// 	},
-
-// };
-
-// const otherPlaylist = Object.assign( {}, playlistApp);
-
-// playlistApp.add('Mr. Brightside', 'The Killers');
-// playlistApp.add('In The End', 'Linkin Park');
-// playlistApp.add('New Slang', 'The Shins');
-// playlistApp.add('Electric Feel', 'MGMT');
-// playlistApp.add('Ms. Jackson', 'OutKast');
-// playlistApp.add('Backyard', 'Guster');
-// playlistApp.add('My Girls', 'Animal Collective');
-// playlistApp.add('Someday', 'The Strokes');
-// playlistApp.add('Feel Good Inc', 'Gorillaz');
-
-
-// playlistApp.complete(1);
-// playlistApp.remove(2);
-// playlistApp.updateSong(4, 'Photograph', 'Nickelback');
-
-// otherPlaylist.add('All The Small Things', 'Blink-182');
 
 
