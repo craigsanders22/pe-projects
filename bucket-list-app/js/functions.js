30924c406004cf4cac7b6dec05ba75662183e231
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2023-10-18 16:41:07 -0500

    .

---
+++ bucket-list-app/js/functions.js
@@ -0,0 +1,19 @@
+// export async function showPage(pageId) {
+//     const pages = document.querySelectorAll('.page');
+//     pages.forEach(page => {
+//         page.style.display = 'none';
+//     });
+
+//     // show the requested page
+//     const page = document.querySelector(`#${pageId}`);
+//     page.style.display = 'block';
+
+//     // dynamically handle the page
+//     if (pageId === 'wayfindingPage') {
+//         const { handleWayfinding } = await import('./wayfinding.js');
+//         handleWayfinding();
+//     } else if (pageId === 'categoryListPage') {
+//         const { handleCategoryList } = await import('./wayfinding.js');
+//         handleCategoryList();
+//     }
+// }
