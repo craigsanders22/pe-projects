69ed72fe459dfa7c741209329c15c8ff20f49941
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2023-11-06 14:27:12 -0500

    resume updates

--- resume/index.php
+++ resume/index.php
@@ -11,185 +11,186 @@
 		</div>
 	</section>
 
-<section class="work-experience">
-	<div class="inner-column">
+	<section class="work-experience">
+		<div class="inner-column">
 
-		<h2 class="loud-voice">Work Experience</h2>
-		<div class="work-details">
+			<h2 class="resume-head loud-voice">Work Experience</h2>
+			<div class="work-details">
 
-			<div class="top-details">
+				<div class="top-details">
 
-				<div class="title-text">
-					<h3 class="company-name attention-voice">Wildwood Market</h3>
-					<h3 class="location strong-voice">
+					<div class="title-text">
+						<h3 class="company-name attention-voice">Wildwood Market</h3>
+						<h3 class="location strong-voice">
 						Indianapolis, IN</h3>
 
-					<div class="title-image">
-						<img src="../assets/cart.png" alt="">
-						<h3 class="work-title attention-voice ">Small Business Owner / Operator</h3>
+						<div class="title-image">
+							<img src="../assets/cart.png" alt="">
+							<h3 class="work-title attention-voice ">Small Business Owner / Operator</h3>
+						</div>
+
 					</div>
-
 				</div>
-			</div>
 
-			<div class="work-dates strong-voice">September 2014 - April 2023</div>
+				<div class="degree strong-voice">September 2014 - April 2023</div>
 
-			<div class="role-text">
-				<ul>
-					<li class="calm-voice">Established and managed a successful family-owned specialty market, supporting local farmers and food brands.</li>
+				<div class="role-text">
+					<ul>
+						<li class="calm-voice">Established and managed a successful family-owned specialty market, supporting local farmers and food brands.</li>
 
-					<li class="calm-voice">Curated a thoughtful selection of fresh and packaged food products in the Fountain Square neighborhood of Indianapolis.</li>
+						<li class="calm-voice">Curated a thoughtful selection of fresh and packaged food products in the Fountain Square neighborhood of Indianapolis.</li>
 
-					<li class="calm-voice">Handled all day-to-day operations including human resources, accounting, marketing, procurement, staff training, social media, and developing recipes.</li>
+						<li class="calm-voice">Handled all day-to-day operations including human resources, accounting, marketing, procurement, staff training, social media, and developing recipes.</li>
 
-					<li class="calm-voice">Maintined a cash-flow positive company with $1.2M+ in yearly sales on less than 1000 square feet of floorspace.</li>
+						<li class="calm-voice">Maintined a cash-flow positive company with $1.2M+ in yearly sales on less than 1000 square feet of floorspace.</li>
 
-				</ul>
-			</div>
-		</div>
-
-		<div class="work-details">
-			<div class="top-details">
-
-				<div class="title-text">
-					<h3 class="company-name attention-voice">Traders Point Creamery</h3>
-
-					<h3 class="location strong-voice">
-						Zionsville, IN</h3>
-
-					<div class="title-image">
-						<img src="../assets/networking.png" alt="">
-						<h3 class="work-title attention-voice">Director of Operations</h3>
-					</div>
-
+					</ul>
 				</div>
-			</div>
-
-			<div class="work-dates strong-voice">October 2010 - September 2014</div>
-
-			<div class="role-text">
-				<ul>
-					<li class="calm-voice">Oversaw all sales and distribution operations for the product divison of the company.</li>
-					<li class="calm-voice">Steered a rebranding effort for more than 20 products and successfully navigated launch of rebrand with distrubutors, retailers, and the public.</li>
-					<li class="calm-voice">Was liason between marketing, production, ownership, human resources, and other departments.</li>
-					<li class="calm-voice">Headed up projects to develop innovative products and leveraged relationships to achieve national distribution of new products.</li>
-
-				</ul>
 			</div>
 
 			<div class="work-details">
 				<div class="top-details">
 
-					<div class="title-image">
-						<img src="../assets/growth.png" alt="">
-						<h3 class="work-title attention-voice">Key Account Sales Manager</h3>
-					</div>
+					<div class="title-text">
+						<h3 class="company-name attention-voice">Traders Point Creamery</h3>
 
+						<h3 class="location strong-voice">
+						Zionsville, IN</h3>
+
+						<div class="title-image">
+							<img src="../assets/networking.png" alt="">
+							<h3 class="work-title attention-voice">Director of Operations</h3>
+						</div>
+
+					</div>
 				</div>
 
-				<div class="work-dates strong-voice">June 2007 - August 2010</div>
+				<div class="strong-voice degree">October 2010 - September 2014</div>
 
 				<div class="role-text">
 					<ul>
-						<li class="calm-voice">Helped to transform a local organic dairy brand into a nationally distributed product available in grocery and specialty chains.</li>
-						<li class="calm-voice">Cultivated and maintained relationships with national distributors, leading to increased market presence.</li>
-						<li class="calm-voice">Met with key national retailers to present products and negotiate placement on shelves. </li>
-
-					</ul>
-				</div>
-			</div>
-		</div>
-
-	</div>
-
-</section>
-
-<section class="education">
-	<div class="inner-column">
-
-		<h2 class="loud-voice">Education</h2>
-
-		<div class="education-intro">
-			<div class="title-image">
-				<img src="../assets/rocket.png" alt="">
-			</div>
-
-			<div class="attention-voice school">Perpetual Education - Design for the Web
-			</div>
-		</div>
-
-		<div class="education-text">
-			<div class="strong-voice degree">Web Design Mentorship, 2023</div>
-
-		</div>
-		<div class="role-text">
-					<ul>
-						<li class="calm-voice">A 9 month mentorship course focusing on full stack web development and design concepts</li>
-						<li class="calm-voice">A holistic approach to development and design which teaches students about work-life balance, UX, acccesability, teamwork, developing goals, researching, and more.</li>
-						<li class="calm-voice">Became proficient in HTML, CSS, PHP, WordPress, JavaScript, Vue, Node.js. </li>
-						<li class="calm-voice">Used design and development tools like Figma, Affinity Designer, Sketch, MAMP, CodePen, and much more!</li>
+						<li class="calm-voice">Oversaw all sales and distribution operations for the product divison of the company.</li>
+						<li class="calm-voice">Steered a rebranding effort for more than 20 products and successfully navigated launch of rebrand with distrubutors, retailers, and the public.</li>
+						<li class="calm-voice">Was liason between marketing, production, ownership, human resources, and other departments.</li>
+						<li class="calm-voice">Headed up projects to develop innovative products and leveraged relationships to achieve national distribution of new products.</li>
 
 					</ul>
 				</div>
 
-		<div class="education-intro">
-			<div class="title-image">
-				<img src="../assets/exam.png" alt="">
-			</div>
+				<div class="work-details">
+					<div class="top-details">
 
-			<div class="attention-voice school">Indiana University - Indianapolis
-			</div>
-		</div>
+						<div class="title-image">
+							<img src="../assets/growth.png" alt="">
+							<h3 class="work-title attention-voice">Key Account Sales Manager</h3>
+						</div>
 
-		<div class="education-text">
-			<div class="strong-voice degree">Bachelor of Arts in English, 2007</div>
+					</div>
 
-		</div>
+					<div class="degree strong-voice">June 2007 - August 2010</div>
 
-	</div>
-</section>
+					<div class="role-text">
+						<ul>
+							<li class="calm-voice">Helped to transform a local organic dairy brand into a nationally distributed product available in grocery and specialty chains.</li>
+							<li class="calm-voice">Cultivated and maintained relationships with national distributors, leading to increased market presence.</li>
+							<li class="calm-voice">Met with key national retailers to present products and negotiate placement on shelves. </li>
 
-<section class="skills">
-	<div class="education-intro">
-	<div class="inner-column">
-
-		<div class="attention-voice school">Skills
-			</div>
-		</div>
-
-		<div class="education-text">
-			<div class="strong-voice degree">Just a few of them! I'd love to discuss how my unique skill set could complement you or your company!</div>
-
-		</div>
-		<div class="role-text">
-					<ul>
-						<li class="calm-voice">A 9 month mentorship course focusing on full stack web development and design concepts</li>
-						<li class="calm-voice">A holistic approach to development and design which teaches students about work-life balance, UX, acccesability, teamwork, developing goals, researching, and more.</li>
-						<li class="calm-voice">Became proficient in HTML, CSS, PHP, WordPress, JavaScript, Vue, Node.js. </li>
-						<li class="calm-voice">Used design and development tools like Figma, Affinity Designer, Sketch, MAMP, CodePen, and much more!</li>
-
-					</ul>
+						</ul>
+					</div>
 				</div>
+			</div>
 
-</div>
-</section>
+		</div>
+
+	</section>
+
+	<section class="education">
+		<div class="inner-column">
+			<h2 class="resume-head loud-voice">Education</h2>
+			<div class="education-intro">
+				<div class="title-image">
+					<img src="../assets/rocket.png" alt="">
+				</div>
+				<div class="attention-voice school">Perpetual Education - Design for the Web
+				</div>
+			</div>
+			<div class="education-text">
+				<div class="strong-voice degree">Web Design Mentorship, 2023</div>
+			</div>
+			<div class="role-text">
+				<ul>
+					<li class="calm-voice">A 9 month mentorship course focusing on full stack web development and design concepts</li>
+					<li class="calm-voice">A holistic approach to development and design which teaches students about work-life balance, UX, acccesability, teamwork, developing goals, researching, and more.</li>
+					<li class="calm-voice">Became proficient in HTML, CSS, PHP, WordPress, JavaScript, Vue, Node.js.</li>
+					<li class="calm-voice">Used design and development tools like Figma, Affinity Designer, Sketch, MAMP, CodePen, and much more!</li>
+				</ul>
+			</div>
+			<div class="education-intro">
+				<div class="title-image">
+					<img src="../assets/exam.png" alt="">
+				</div>
+				<div class="attention-voice school">Indiana University - Indianapolis
+				</div>
+			</div>
+			<div class="education-text">
+				<div class="strong-voice degree">Bachelor of Arts in English, 2007</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="skills">
+		<div class="inner-column">
+			<h2 class="resume-head loud-voice">Skills</h2> 
+			<div class="education-text">
+				<div class="strong-voice degree">Just a few of them! I'd love to discuss how my unique skill set could complement you or your company!</div>
+			</div>
+			<div class="skills-intro">
+				<div class="title-image">
+					<img src="../assets/briefcase.png" alt="">
+				</div>
+				<div class="attention-voice skills-type">Professional
+				</div>
+			</div>
+			<!-- <div class="strong-voice skills-text">Just a few of them! I'd love to discuss how my unique skill set could complement you or your company!</div> -->
+			<div class="skills-text">
+				<ul>
+					<li class="calm-voice">Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+					<li class=" calm-voice">Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+					<li class="calm-voice">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, sint.</li>
+					<li class="calm-voice">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius, porro!</li>
+						<li class="calm-voice">Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+					<li class=" calm-voice">Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+					<li class="calm-voice">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, sint.</li>
+					<li class="calm-voice">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius, porro!</li>
+
+				</ul>
+			</div>
 
 
+			<div class="skills-intro">
+				<div class="title-image">
+					<img src="../assets/web.png" alt="">
+				</div>
+				<div class="attention-voice skills-type">Technical
+				</div>
+			</div>
+			
+			<div class="skills-text">
+				<ul>
+					<li class="calm-voice">Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+					<li class=" calm-voice">Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+					<li class="calm-voice">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, sint.</li>
+					<li class="calm-voice">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius, porro!</li>
+						<li class="calm-voice">Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+					<li class=" calm-voice">Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+					<li class="calm-voice">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, sint.</li>
+					<li class="calm-voice">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius, porro!</li>
 
+				</ul>
+			</div>
+		</div>
+	</section>
 
-
-
-		<h2 class="attention-voice">Skills</h2>
-
-		<ul class="skills-list">
-			<li class="skills-item">Relationship Building and Sales</li>
-			<li class="skills-item">Team Leadership and Management</li>
-			<li class="skills-item">Budgeting and Forecasting</li>
-			<li class="skills-item">Sales and Marketing Strategy</li>
-		</ul>
-	
-	</div>
-</section>
 </div>
 
 </main>
