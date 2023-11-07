7fee80a63226889dc9da6f58889c4da16b37616e
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2023-11-07 11:50:07 -0500

    .

--- projects/project-header.php
+++ projects/project-header.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../forms/forms.css">
-     <link rel="stylesheet" href="../styles/projects.css">
+     <!-- <link rel="stylesheet" href="../styles/projects.css"> -->
     
 </head>
 
