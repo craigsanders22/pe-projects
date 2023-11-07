632a9efaff1d2181b33143224ad9203f632ac907
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2023-11-07 11:04:30 -0500

    button flap on about page

--- aboutme/index.php
+++ aboutme/index.php
@@ -1,12 +1,106 @@
 <?php include('../header.php');?>
 
+<style>
+    .about-container {
 
-		
-    <nav class="about-nav">
+    perspective: 800px;
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 400px;
+    padding: 25px;
+}
+
+.toggle {
+    position: relative;
+    border: 2px solid var(--main-dark);
+    border-radius: var(--border-radius);
+    transition: transform cubic-bezier(0, 0, 0.30, 2) .4s;
+    transform-style: preserve-3d;
+    perspective: 800px;
+}
+
+.toggle>input[type="radio"] {
+    display: none;
+}
+
+.toggle>#goalButton:checked~#flap {
+    transform: rotateY(-180deg);
+}
+
+.toggle>#goalButton:checked~#flap>.content {
+    transform: rotateY(-180deg);
+}
+
+.toggle>#resumeButton:checked~#flap {
+    transform: rotateY(0deg);
+}
+
+.toggle>label {
+    display: inline-block;
+    min-width: 170px;
+    
+    padding: 30px;
+
+    text-align: center;
+    color: var(--main-white);
+    cursor: pointer;
+}
+
+.toggle>label,
+.toggle>#flap {
+    font-weight: bold;
+    text-transform: capitalize;
+}
+
+.toggle>#flap {
+    position: absolute;
+    top: calc( 0px - var(--border-width));
+    left: 50%;
+    height: calc(100% + var(--border-width) * 2);
+    width: 60%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  
+    background-color: var(--main-gray);
+    border-top-right-radius: var(--border-radius);
+    border-bottom-right-radius: var(--border-radius);
+    transform-style: preserve-3d;
+    transform-origin: left;
+    transition: transform cubic-bezier(0.4, 0, 0.2, 1) .5s;
+}
+
+.toggle>#flap>.content {
+    color: #333;
+    transition: transform 0s linear .25s;
+    transform-style: preserve-3d;
+}
+</style>
+
+<div class="about-container">
+
+        <form class="toggle">
+           
+            <input type="radio" id="goalButton" name="choice" value="goals">
+            <label for="goalButton">GOALS</label>
+
+            <input type="radio" id="resumeButton" name="choice" value="resume">
+            <label for="resumeButton">RESUME</label>
+
+            <div id="flap"><span class="content">switch</span></div>
+
+        </form>
+
+    </div>
+
+
+
+
+ <!--    <nav class="about-nav">
         <button id="goalButton">Goals</button>
         <button id="resumeButton">Resume</button>
     </nav>
-
+ -->
 
     <div id="goals" class="page">
         <?php include('goals.php');?>
--- aboutme/scripts.js
+++ aboutme/scripts.js
@@ -8,4 +8,46 @@ window.onload = function() {
         document.getElementById("resume").style.display = "block";
         document.getElementById("goals").style.display = "none";
     });
+    
+    // programmatically click the resumeButton
+    document.getElementById("resumeButton").click();
 };
+
+
+
+const st = {};
+
+st.flap = document.querySelector('#flap');
+st.toggle = document.querySelector('.toggle');
+
+st.choice1 = document.querySelector('#goalButton');
+st.choice2 = document.querySelector('#resumeButton');
+
+st.flap.addEventListener('transitionend', () => {
+
+    if (st.choice1.checked) {
+        st.toggle.style.transform = 'rotateY(-15deg)';
+        setTimeout(() => st.toggle.style.transform = '', 400);
+    } else {
+        st.toggle.style.transform = 'rotateY(15deg)';
+        setTimeout(() => st.toggle.style.transform = '', 400);
+    }
+
+})
+
+st.clickHandler = (e) => {
+
+    if (e.target.tagName === 'LABEL') {
+        setTimeout(() => {
+            st.flap.children[0].textContent = e.target.textContent;
+        }, 250);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    st.flap.children[0].textContent = st.choice2.nextElementSibling.textContent;
+});
+
+document.addEventListener('click', (e) => st.clickHandler(e));
+
+
