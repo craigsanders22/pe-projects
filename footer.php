0685256e33c00620fe9b29a993f41c93d904f889
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2023-11-09 14:26:42 -0500

    .

--- footer.php
+++ footer.php
@@ -13,21 +13,22 @@
        </picture>
      </div>
     
-     <nav class="site-nav">
-       <h3 class='strong-voice'>
-       <div class="nav-list">
-         <div><a href="projects/">projects</a></div>
-        <div><a href="aboutme/">about me</a></div>
-         
-         <div><a href="contact/">let's chat</a> </div>
-         <div><a href="archive">the vault</a></div>
-       </div>
-     </h3>
-     </nav>
-     
+            <nav class="site-nav">
+  <h3 class='calm-voice'>
+    <div class="nav-list">
+      <div><a href="projects/">projects</a></div>
+      <div><a href="aboutme/">about me</a></div>
+    
+      <div><a href="contact/">let's chat</a> </div>
+      <div><a href="archive">the vault</a></div>
+    </div>
+  </h3>
+</nav>
+
    </div>
   </div>
 </footer>
 
 </body>
 </html>
+
